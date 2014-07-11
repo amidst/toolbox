4b2069c24d367d0c9ae2221ba70f8c972f72b611
andresmasegosa <andresmasegosa@gmail.com>
2014-07-11 15:54:07 +0200

    Included a different directory for the implementations. Added the class BNFactory for creating BNs.

--- src/eu/amidst/core/StaticBayesianNetwork/Classifier.java
+++ src/eu/amidst/core/StaticBayesianNetwork/Classifier.java
@@ -5,6 +5,6 @@ import eu.amidst.core.StaticDataBase.DataInstance;
 /**
  * Created by afa on 02/07/14.
  */
-public interface Classifier extends BayesianNetwork {
+public interface Classifier {
     void predict(DataInstance instance);
 }
--- src/eu/amidst/core/StaticBayesianNetwork/NaiveBayes.java
+++ src/eu/amidst/core/StaticBayesianNetwork/NaiveBayes.java
@@ -5,7 +5,7 @@ import eu.amidst.core.StaticDataBase.DataInstance;
 /**
  * Created by afa on 02/07/14.
  */
-public class NaiveBayes implements Classifier {
+public class NaiveBayes implements BayesianNetwork, Classifier {
     @Override
     public void predict(DataInstance instance) {
 
