6d123191f971827628c25aff958008b749327f13
andresmasegosa <andresmasegosa@gmail.com>
2014-08-29 08:53:27 +0200

    Some major changes

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,43 @@
-import eu.amidst.core.StaticBayesianNetwork.BNFactory;
-import eu.amidst.core.StaticBayesianNetwork.BayesianNetwork;
-import eu.amidst.core.StaticBayesianNetwork.impl.BayesNet;
+import eu.amidst.core.StaticDataBase.DataStream;
+import eu.amidst.core.StaticDataBase.readers.DataStreamReaderFromFile;
+import eu.amidst.models.staticmodels.NaiveBayesClusteringModel;
+import eu.amidst.models.staticmodels.NaiveBayes;
 
 public class Main {
 
+    public static void learningNaiveBayesClusteringModel(){
+
+        DataStreamReaderFromFile reader = new DataStreamReaderFromFile("./data/data.arff");
+
+        DataStream dataStream = reader.getDataStream();
+
+        NaiveBayesClusteringModel clusteringModel = new NaiveBayesClusteringModel();
+
+        clusteringModel.buildStructure(dataStream.getStaticDataHeader());
+        clusteringModel.initLearning();
+        clusteringModel.learnModelFromStream(dataStream);
+
+        dataStream.restart();
+        clusteringModel.clusterMemberShip(dataStream.nextDataInstance());
+    }
+
+    public static void learningNaiveBayes(){
+
+        DataStreamReaderFromFile reader = new DataStreamReaderFromFile("./data/data.arff");
+
+        DataStream dataStream = reader.getDataStream();
+
+        NaiveBayes nb = new NaiveBayes();
+        nb.setClassVarID(dataStream.getStaticDataHeader().getObservedVariables().size()-1);
+        nb.buildStructure(dataStream.getStaticDataHeader());
+        nb.initLearning();
+        nb.learnModelFromStream(dataStream);
+
+        dataStream.restart();
+        nb.predict(dataStream.nextDataInstance());
+    }
+
     public static void main(String[] args) {
-
-        BayesianNetwork myNet;
-
-        myNet = BNFactory.createBN();
-
-
-
 
         System.out.println("Hello World!");
     }
