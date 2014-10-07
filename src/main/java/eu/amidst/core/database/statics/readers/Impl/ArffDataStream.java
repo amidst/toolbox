53e4f6925fe6d45b70dcc509786cd1cf3789fd59
Sigve Hovda <sigveh@verdandetechnology.com>
2014-10-07 14:34:41 +0200

    refactor on folder structure again, some strange maven stuff

---
+++ src/main/java/eu/amidst/core/database/statics/readers/Impl/ArffDataStream.java
@@ -0,0 +1,59 @@
+package eu.amidst.core.database.statics.readers.Impl;
+
+import eu.amidst.core.database.statics.DataStream;
+import eu.amidst.core.header.statics.StaticDataHeader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+
+/**
+ * Created by sigveh on 10/7/14.
+ */
+public class ArffDataStream implements DataStream{
+    private Iterator iterator;
+    private StaticDataHeader staticDataHeader;
+    private int rows;
+    private int cols;
+    private double[][] data;
+
+    public ArffDataStream(String fileName) throws FileNotFoundException,
+                IOException {
+            CSVReader reader = new CSVReader(new FileReader(fileName));
+
+            List<String []> xrows = reader.readAll();
+            rows =  xrows.size();
+            cols = xrows.get(0).length;
+
+            data = new double[rows][cols];
+            int j = 0;
+            for (String[] row : xrows) {
+                for (int i = 0; i < row.length; i++) {
+                    data[j][i] = Double.parseDouble(row[i]);
+                }
+                j = j+1;
+            }
+            reader.close();
+    }
+
+    @Override
+    public void restart() {
+        iterator.remove();
+        //TODO reload iterator
+    }
+
+    @Override
+    public StaticDataHeader getStaticDataHeader() {
+        return staticDataHeader;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return iterator;
+    }
+}
