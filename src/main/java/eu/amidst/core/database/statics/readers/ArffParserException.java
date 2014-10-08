d9b3c4ef0df8259b5545bbef33007a0ffa932400
Sigve Hovda <sigveh@verdandetechnology.com>
2014-10-08 11:54:56 +0200

    Added exceptions, compacted parser code

--- src/main/java/eu/amidst/core/database/statics/readers/ArffParserException.java
+++ src/main/java/eu/amidst/core/database/statics/readers/ArffParserException.java
@@ -6,4 +6,7 @@ import java.io.IOException;
  * Created by sigveh on 10/8/14.
  */
 public class ArffParserException extends IOException {
+    public ArffParserException(String message) {
+        super(message);
+    }
 }
