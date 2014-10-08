e75fba693d2a3e394bf68d24356834a39e6fca07
Sigve Hovda <sigveh@verdandetechnology.com>
2014-10-08 11:04:43 +0200

    JUnit4 style tests, added ArffParserException

--- src/test/java/eu/amidst/core/database/statics/readers/Impl/ArffDataStreamTest.java
+++ src/test/java/eu/amidst/core/database/statics/readers/Impl/ArffDataStreamTest.java
@@ -1,9 +1,13 @@
 package eu.amidst.core.database.statics.readers.Impl;
 
 import eu.amidst.core.database.statics.DataStream;
-import junit.framework.TestCase;
+//import junit.framework.TestCase;
+import eu.amidst.core.database.statics.readers.ArffParserException;
+import org.junit.Test;
 
-public class ArffDataStreamTest extends TestCase {
+import java.util.ArrayList;
+
+public class ArffDataStreamTest {
 
     public void testRestart() throws Exception {
 
@@ -13,31 +17,33 @@ public class ArffDataStreamTest extends TestCase {
 
     }
 
+    @Test
     public void testConstructor1() throws Exception {
         DataStream dataStream = new ArffDataStream("data/arff/hayTrain.arff");
     }
 
-
+    @Test
     public void testConstructor2() throws Exception {
         DataStream dataStream = new ArffDataStream("data/arff/testCapitals.arff");
     }
 
+    @Test
     public void testConstructor3() throws Exception {
 
         DataStream dataStream = new ArffDataStream("data/arff/testSpacesAndTabs.arff");
     }
 
-
+    @Test(expected=NullPointerException.class)
     public void testConstructor4() throws Exception {
 
         DataStream dataStream = new ArffDataStream("data/arff/testWrongRelationName.arff");
     }
 
+    @Test(expected=ArffParserException.class)
     public void testConstructor5() throws Exception {
 
         DataStream dataStream = new ArffDataStream("data/arff/testWrongAttributeName.arff");
     }
-
 
 
     //System.out.println(" dataStream has iterator: " );
