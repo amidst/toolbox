8d2c446cfabec3063a690cdf6cdbf9e2eb8bfc72
Sigve Hovda <sigveh@verdandetechnology.com>
2014-10-08 10:41:40 +0200

    before testing parser

--- src/test/java/eu/amidst/core/database/statics/readers/Impl/ArffDataStreamTest.java
+++ src/test/java/eu/amidst/core/database/statics/readers/Impl/ArffDataStreamTest.java
@@ -13,13 +13,32 @@ public class ArffDataStreamTest extends TestCase {
 
     }
 
-    public void testIterator() throws Exception {
-
-        System.out.println(" dataStream has iterator: " );
+    public void testConstructor1() throws Exception {
         DataStream dataStream = new ArffDataStream("data/arff/hayTrain.arff");
-
-
-
-
     }
+
+
+    public void testConstructor2() throws Exception {
+        DataStream dataStream = new ArffDataStream("data/arff/testCapitals.arff");
+    }
+
+    public void testConstructor3() throws Exception {
+
+        DataStream dataStream = new ArffDataStream("data/arff/testSpacesAndTabs.arff");
+    }
+
+
+    public void testConstructor4() throws Exception {
+
+        DataStream dataStream = new ArffDataStream("data/arff/testWrongRelationName.arff");
+    }
+
+    public void testConstructor5() throws Exception {
+
+        DataStream dataStream = new ArffDataStream("data/arff/testWrongAttributeName.arff");
+    }
+
+
+
+    //System.out.println(" dataStream has iterator: " );
 }
