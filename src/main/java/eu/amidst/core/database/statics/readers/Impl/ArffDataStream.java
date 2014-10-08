b0d59dc165063b13105fbb7acb348d658ca00884
Sigve Hovda <sigveh@verdandetechnology.com>
2014-10-08 12:32:39 +0200

    Used tokenizer in parser class

--- src/main/java/eu/amidst/core/database/statics/readers/Impl/ArffDataStream.java
+++ src/main/java/eu/amidst/core/database/statics/readers/Impl/ArffDataStream.java
@@ -10,6 +10,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import au.com.bytecode.opencsv.CSVReader;
 
@@ -23,36 +24,51 @@ public class ArffDataStream implements DataStream{
     private int rowSize;
     private int columnSize;
     private double[][] data;
+    private String relation;
 
     public ArffDataStream(String fileName) throws FileNotFoundException,
             IOException, ArffParserException {
         CSVReader reader = new CSVReader(new FileReader(fileName));
         List<String []> lines = reader.readAll();
-
         int headerIndex = 0;
         boolean isHeader = true;
         while (isHeader) {
-            String[] line = lines.get(headerIndex);
-            String firstWord = line[0].trim().toLowerCase();
-            if ( firstWord.isEmpty()) {
-                headerIndex = headerIndex+1;
-                System.out.println("Blank line   " + headerIndex);
-            } else if ( firstWord.startsWith("%") ) {
+            String[] lineAsArray = lines.get(headerIndex);
+            String line = lineArrayToLine( lineAsArray );
+
+            //String firstWord = lineAsArray[0].trim().toLowerCase();
+            StringTokenizer st = new StringTokenizer( line );
+            if( !st.hasMoreTokens() ) {
                 headerIndex = headerIndex + 1;
-                System.out.println("Comment line   " + headerIndex);
-            } else if ( firstWord.startsWith("@relation ") ) {
-                headerIndex = headerIndex+1;
-                System.out.println("Relation line   " + headerIndex);
-            } else if ( firstWord.startsWith("@attribute ") ) {
-                headerIndex = headerIndex+1;
-                System.out.println("Attribute line   " + headerIndex);
-            } else if ( firstWord.equals("@data") ) {
-                headerIndex = headerIndex+1;
-                System.out.println("Data line   " + headerIndex);
-            } else if ( firstWord.startsWith("@") ) {
-                throw new ArffParserException( "Illegal header element: " + "'" + line[0].trim() + "'" ) ;
-            } else {
-                isHeader = false;
+                System.out.println("Blank line   " + headerIndex);
+            }
+            else {
+                String firstWord = st.nextToken().toLowerCase();
+                if (firstWord.startsWith("%")) {
+                    headerIndex = headerIndex + 1;
+                    System.out.println("Comment line   " + headerIndex);
+                } else if (firstWord.equals("@relation")) {
+                    headerIndex = headerIndex + 1;
+                    System.out.println("Relation line   " + headerIndex);
+                    System.out.println( "    - relation stuff:");
+                    while( st.hasMoreTokens() ) {
+                        System.out.println( "                 " + st.nextToken() );
+                    }
+                } else if (firstWord.equals("@attribute")) {
+                    headerIndex = headerIndex + 1;
+                    System.out.println("Attribute line   " + headerIndex);
+                    System.out.println( "    - attribute stuff:");
+                    while( st.hasMoreTokens() ) {
+                        System.out.println( "                 " + st.nextToken() );
+                    }
+                } else if (firstWord.equals("@data")) {
+                    headerIndex = headerIndex + 1;
+                    System.out.println("Data line   " + headerIndex);
+                } else if (firstWord.startsWith("@")) {
+                    throw new ArffParserException("Illegal header element: " + "'" + line + "'");
+                } else {
+                    isHeader = false;
+                }
             }
         }
 
@@ -71,6 +87,17 @@ public class ArffDataStream implements DataStream{
         reader.close();
     }
 
+    public static String lineArrayToLine(String[] lineAsArray) {
+        String line = "";
+        if (lineAsArray.length == 0) {
+            return null;
+        }
+        for (int i = 0; i < lineAsArray.length; ++i) {
+            if( i > 0 ) { line = line + ","; }
+            line = line + lineAsArray[i];
+        }
+        return line;
+    }
 
 
     @Override
