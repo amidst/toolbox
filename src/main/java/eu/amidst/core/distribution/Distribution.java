7ea84568f63a89973094857f777a36bf7621da98
Antonio Fernández <afalvarez@ual.es>
2014-11-03 19:41:33 +0100

    Some advances on the distribution package

--- src/main/java/eu/amidst/core/distribution/Distribution.java
+++ src/main/java/eu/amidst/core/distribution/Distribution.java
@@ -1,31 +1,13 @@
 package eu.amidst.core.distribution;
 
-import eu.amidst.core.database.statics.readers.DataInstance;
-import eu.amidst.core.potential.Potential;
+import eu.amidst.core.header.statics.Variable;
 
 /**
  * Created by afa on 02/07/14.
  */
 public interface Distribution {
 
-    Potential getRestrictedPotentialExceptFor(DataInstance instance, int varID);
+    public Variable getVariable();
 
-    Potential getRestrictedPotential(DataInstance instance);
 
-    public double getProbability(DataInstance data);
-
-    public void setExpectationParameters(ExpectationParameters ss);
-
-    public ExpectationParameters getExpectationParameters();
-
-    public interface ExpectationParameters {
-        public double[] getExpectationParameters();
-
-    }
-
-    //public double[] getNaturalParameters();
-
-    //public double[] getStandardParameters();
-
-    //public void setStandardParameters(double[] par);
-  }
+}
