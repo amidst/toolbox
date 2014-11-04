a629d21712bad51be0153d87ed619436e3ffd7a5
Antonio Fernández <afalvarez@ual.es>
2014-11-04 13:28:28 +0100

    Multinomial, Normal and CLG distributions

--- src/main/java/eu/amidst/core/distribution/Normal.java
+++ src/main/java/eu/amidst/core/distribution/Normal.java
@@ -9,14 +9,9 @@ public class Normal implements UnivariateDistribution {
 
     private double mean;
 
-    // sd or precision??? Both are redundant
     private double sd;
-    private double precision;
 
-    private double sumX;
-    private double sumX2;
     private Variable var;
-
 
     public Normal(Variable var) {
         this.var = var;
@@ -28,10 +23,6 @@ public class Normal implements UnivariateDistribution {
         return mean;
     }
 
-    public double getPrecision() {
-        return precision;
-    }
-
     public double getSd() {
         return sd;
     }
@@ -40,9 +31,6 @@ public class Normal implements UnivariateDistribution {
         this.mean = mean;
     }
 
-    public void setPrecision(double precision) {
-        this.precision = precision;
-    }
     public void setSd(double sd) {
         this.sd = sd;
     }
@@ -61,12 +49,4 @@ public class Normal implements UnivariateDistribution {
     public double getProbability(double x) {
         return(1/(sd * Math.sqrt(2 * Math.PI)) * Math.exp(-0.5 * Math.pow(((x-mean)/sd),2)));
     }
-
-    @Override
-    public void updateCounts(double data) {
-        this.sumX = sumX + data;
-        this.sumX2 = sumX2 + data*data;
-    }
-
-
 }
