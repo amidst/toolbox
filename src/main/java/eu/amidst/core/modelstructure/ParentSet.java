01d45488706996552ea31dcd60e37f5d4baab00d
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2014-11-04 13:21:59 +0100

    Some changes

--- src/main/java/eu/amidst/core/modelstructure/ParentSet.java
+++ src/main/java/eu/amidst/core/modelstructure/ParentSet.java
@@ -2,10 +2,14 @@ package eu.amidst.core.modelstructure;
 
 import eu.amidst.core.header.statics.Variable;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by afa on 02/07/14.
  */
-public interface ParentSet {
-    public void addParent(Variable variable);
+public interface ParentSet<E extends Variable> {
+    public void addParent(E variable);
+    public List<E> getParents();
     public int getNumberOfParents();
 }
--- src/main/java/eu/amidst/core/modelstructure/statics/impl/ParentSetImpl.java
+++ src/main/java/eu/amidst/core/modelstructure/statics/impl/ParentSetImpl.java
@@ -3,14 +3,23 @@ package eu.amidst.core.modelstructure.statics.impl;
 import eu.amidst.core.header.statics.Variable;
 import eu.amidst.core.modelstructure.ParentSet;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by afa on 02/07/14.
  */
-public class ParentSetImpl implements ParentSet{
-    private int[] parentIDs;
+public class ParentSetImpl implements ParentSet<Variable>{
+    private ArrayList<Variable> vars;
 
     public void addParent(Variable variable){
+        vars.add(variable);
+    }
 
+    @Override
+    public List<Variable> getParents() {
+        return vars;
     }
 
     @Override
