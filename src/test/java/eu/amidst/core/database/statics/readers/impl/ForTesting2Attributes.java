f6b24494886ce016aa72316b4b9a5c4c0f5c268b
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2014-11-06 09:57:31 +0100

    Update

--- src/test/java/eu/amidst/core/database/statics/readers/impl/ForTesting2Attributes.java
+++ src/test/java/eu/amidst/core/database/statics/readers/impl/ForTesting2Attributes.java
@@ -3,7 +3,7 @@ package eu.amidst.core.database.statics.readers.impl;
 import com.google.common.collect.ImmutableSet;
 import eu.amidst.core.database.statics.readers.Attribute;
 import eu.amidst.core.database.statics.readers.Attributes;
-import eu.amidst.core.database.statics.readers.Kind;
+import eu.amidst.core.database.statics.readers.StateSpaceType;
 
 import java.util.Set;
 
@@ -12,9 +12,9 @@ import java.util.Set;
  */
 public class ForTesting2Attributes implements Attributes {
 
-    private final Attribute CLASS = new Attribute(0, "CLASS", "NA", Kind.INTEGER);
-    private final Attribute TWO_NAMES = new Attribute(0, "TWO NAMES", "NA", Kind.REAL);
-    private final Attribute THREE_NAMES_HERE = new Attribute(1, "THREE NAMES HERE", "NA", Kind.REAL);
+    private final Attribute CLASS = new Attribute(0, "CLASS", "NA", StateSpaceType.INTEGER);
+    private final Attribute TWO_NAMES = new Attribute(0, "TWO NAMES", "NA", StateSpaceType.REAL);
+    private final Attribute THREE_NAMES_HERE = new Attribute(1, "THREE NAMES HERE", "NA", StateSpaceType.REAL);
 
     private static Set<Attribute> attributes;
     {
