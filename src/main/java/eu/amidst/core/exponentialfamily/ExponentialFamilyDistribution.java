396e44d2b55c151af843f8656e628fdb51146f5b
Antonio Fernández <afalvarez@ual.es>
2014-11-06 17:28:33 +0100

    Files with errors in the code are commented to have a runnable version of the project.

--- src/main/java/eu/amidst/core/exponentialfamily/ExponentialFamilyDistribution.java
+++ src/main/java/eu/amidst/core/exponentialfamily/ExponentialFamilyDistribution.java
@@ -1,12 +1,15 @@
+/*
 package eu.amidst.core.exponentialfamily;
 
 import eu.amidst.core.database.statics.readers.DataInstance;
 import eu.amidst.core.distribution.Distribution;
 import eu.amidst.core.potential.Potential;
 
+*/
 /**
  * Created by afa on 09/10/14.
- */
+ *//*
+
 public interface ExponentialFamilyDistribution extends Distribution {
 
     public SufficientStatistics getSufficientStatistics(DataInstance instance);
@@ -18,3 +21,4 @@ public interface ExponentialFamilyDistribution extends Distribution {
 
     }
 }
+*/
--- src/test/java/eu/amidst/core/distribution/AttributesForTesting.java
+++ src/test/java/eu/amidst/core/distribution/AttributesForTesting.java
@@ -3,15 +3,16 @@ package eu.amidst.core.distribution;
 import com.google.common.collect.ImmutableSet;
 import eu.amidst.core.database.statics.readers.Attribute;
 import eu.amidst.core.database.statics.readers.Attributes;
-import eu.amidst.core.database.statics.readers.Kind;
+
+import eu.amidst.core.database.statics.readers.StateSpaceType;
 
 import java.util.Set;
 
 public class AttributesForTesting implements Attributes {
 
-    private final Attribute X = new Attribute(0, "X", "NA", Kind.INTEGER);
-    private final Attribute Y = new Attribute(1, "Y", "NA", Kind.INTEGER);
-    private final Attribute Z = new Attribute(2, "Z", "NA", Kind.INTEGER);
+    private final Attribute X = new Attribute(0, "X", "NA", StateSpaceType.INTEGER);
+    private final Attribute Y = new Attribute(1, "Y", "NA", StateSpaceType.INTEGER);
+    private final Attribute Z = new Attribute(2, "Z", "NA", StateSpaceType.INTEGER);
 
     private static Set<Attribute> attributes;
     {
