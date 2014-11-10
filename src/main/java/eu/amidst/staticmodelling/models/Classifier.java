69112615ecca38983be9e79e93d84476005548ea
anamf <ana@h188.aau1x-2.klient.slv.site.aau.dk>
2014-11-10 16:09:00 +0100

    Modifications in (Dynamic)DataInstance

--- src/main/java/eu/amidst/staticmodelling/models/Classifier.java
+++ src/main/java/eu/amidst/staticmodelling/models/Classifier.java
@@ -1,7 +1,7 @@
 package eu.amidst.staticmodelling.models;
 
 
-import eu.amidst.core.database.statics.readers.DataInstance;
+import eu.amidst.core.database.DataInstance;
 
 /**
  * Created by afa on 02/07/14.
