ce08440a12af7da2b1783f584253da04e731ed58
Antonio Fernández <afalvarez@ual.es>
2014-11-12 15:36:46 +0100

    Included some abstract classes/methods in the distribution package. Now it looks much better. Package hugin removed.

--- src/main/java/eu/amidst/core/distribution/Normal_NormalParents.java
+++ src/main/java/eu/amidst/core/distribution/Normal_NormalParents.java
@@ -27,17 +27,7 @@ import java.util.List;
  * @version 1.0
  * @since 2014-11-4
  */
-public class Normal_NormalParents implements ConditionalDistribution {
-
-    /**
-     * The variable of the distribution
-     */
-    private Variable var;
-
-    /**
-     * The list of parent variables
-     */
-    private List<Variable> parents;
+public class Normal_NormalParents extends ConditionalDistribution {
 
     /**
      * The "intercept" parameter of the distribution
@@ -145,15 +135,6 @@ public class Normal_NormalParents implements ConditionalDistribution {
     }
 
     /**
-     * Gets the set of conditioning variables.
-     * @return A <code>unmodifiable List</code> with the conditioning variables.
-     */
-    @Override
-    public List<Variable> getConditioningVariables() {
-        return parents;
-    }
-
-    /**
      * Evaluates the resulting univariate density function in a point after conditioning the distribution to a
      * given parent <code>Assignment</code>.
      * @param assignment An <code>Assignment</code> for the parents.
@@ -176,15 +157,4 @@ public class Normal_NormalParents implements ConditionalDistribution {
         double value = assignment.getValue(this.var);
         return (getUnivariateNormal(assignment).getLogProbability(value));
     }
-
-    /**
-     * Gets the variable of the distribution.
-     * @return A <code>Variable</code> object.
-     */
-    @Override
-    public Variable getVariable() {
-        return var;
-    }
-
-
 }
