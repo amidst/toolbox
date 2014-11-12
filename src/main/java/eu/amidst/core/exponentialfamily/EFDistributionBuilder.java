ee9d1f57398d4883dc317bb31b00b82502426ab2
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2014-11-12 14:45:43 +0100

    First design attempt of ExponentialFamily

---
+++ src/main/java/eu/amidst/core/exponentialfamily/EFDistributionBuilder.java
@@ -0,0 +1,19 @@
+package eu.amidst.core.exponentialfamily;
+
+import eu.amidst.core.distribution.ConditionalDistribution;
+import eu.amidst.core.distribution.UnivariateDistribution;
+
+/**
+ * Created by andresmasegosa on 12/11/14.
+ */
+public class EFDistributionBuilder {
+
+    public static EFConditionalDistribution newFromConditionalDistribution(ConditionalDistribution dist){
+        return null;
+    }
+
+    public static EFUnivariateDistribution newFromConditionalDistribution(UnivariateDistribution dist){
+        return null;
+    }
+
+}
