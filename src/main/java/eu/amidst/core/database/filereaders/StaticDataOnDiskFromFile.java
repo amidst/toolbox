692a12e7e3cb9cd6cf6f887f37f351974cd1630d
Ana M. Martinez <anam.martinezf@gmail.com>
2014-11-12 15:39:18 +0100

    Modifications in Datasets Management

--- src/main/java/eu/amidst/core/database/filereaders/StaticDataOnDiskFromFile.java
+++ src/main/java/eu/amidst/core/database/filereaders/StaticDataOnDiskFromFile.java
@@ -1,14 +1,34 @@
 package eu.amidst.core.database.filereaders;
 
+import eu.amidst.core.database.Attributes;
+import eu.amidst.core.database.DataInstance;
 import eu.amidst.core.database.DataOnDisk;
+import eu.amidst.core.database.DataOnStream;
 
 /**
  * Created by andresmasegosa on 11/11/14.
  */
-public class StaticDataOnDiskFromFile extends StaticDataOnStreamFromFile implements DataOnDisk{
+public class StaticDataOnDiskFromFile implements DataOnDisk, DataOnStream{
 
-    public StaticDataOnDiskFromFile(DataFileReader reader_) {
-        super(reader_);
+    DataFileReader reader;
+
+    public StaticDataOnDiskFromFile(DataFileReader reader) {
+        this.reader = reader;
+    }
+
+    @Override
+    public DataInstance nextDataInstance() {
+        return new StaticDataInstance(this.reader.nextDataRow());
+    }
+
+    @Override
+    public boolean hasMoreDataInstances() {
+        return reader.hasMoreDataRows();
+    }
+
+    @Override
+    public Attributes getAttributes() {
+        return reader.getAttributes();
     }
 
     @Override
