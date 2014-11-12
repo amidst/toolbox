1c361b33ec5fc915437affc85b2656fd74ca6a6a
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2014-11-12 14:45:43 +0100

    First design attempt of ExponentialFamily

---
+++ src/main/java/eu/amidst/core/utils/Vector.java
@@ -0,0 +1,52 @@
+package eu.amidst.core.utils;
+
+/**
+ * Created by andresmasegosa on 12/11/14.
+ */
+public class Vector {
+
+    double[] array;
+
+    public Vector(int size){
+        this.array = new double[size];
+    }
+
+    public Vector(double[] vec){
+        this.array=vec;
+    }
+
+    public double get(int i){
+        return this.array[i];
+    }
+
+    public void set(int i, double val){
+        this.array[i]=val;
+    }
+
+    public int size(){
+        return this.array.length;
+    }
+
+    public void dotProduct(Vector vec){
+        if (vec.size()!=this.size())
+            throw new IllegalArgumentException("Error in variable Vector. Method dotProduct. The parameter vec has a different size. ");
+
+        for (int i=0; i<this.array.length; i++){
+            this.array[i]=vec.get(i);
+        }
+    }
+
+    public static Vector dotProduct(Vector vec1, Vector vec2){
+        if (vec1.size()!=vec2.size())
+            throw new IllegalArgumentException("Error in variable Vector. Method dotProduct. Parameters have a different size. ");
+
+        Vector newVec = new Vector(vec1.size());
+
+        for (int i=0; i<vec1.size(); i++){
+            newVec.set(i,vec1.get(i)*vec2.size());
+        }
+
+        return newVec;
+    }
+
+}
