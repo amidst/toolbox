514ca9c5efad9b9bbd9089f9bfe4379a18000b28
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2014-11-12 14:45:43 +0100

    First design attempt of ExponentialFamily

---
+++ src/main/java/eu/amidst/core/exponentialfamily/SufficientStatistics.java
@@ -0,0 +1,17 @@
+package eu.amidst.core.exponentialfamily;
+
+import eu.amidst.core.utils.Vector;
+
+/**
+ * Created by andresmasegosa on 12/11/14.
+ */
+public class SufficientStatistics extends Vector {
+
+    public SufficientStatistics(int size) {
+        super(size);
+    }
+
+    public SufficientStatistics(double[] vec) {
+        super(vec);
+    }
+}
