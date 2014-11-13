683abfc6f9b6754f758b051754803ddb01440b0f
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2014-11-13 14:22:40 +0100

    Normal implemented in EF form

--- src/main/java/eu/amidst/core/exponentialfamily/EF_DistributionBuilder.java
+++ src/main/java/eu/amidst/core/exponentialfamily/EF_DistributionBuilder.java
@@ -1,9 +1,6 @@
 package eu.amidst.core.exponentialfamily;
 
-import eu.amidst.core.distribution.ConditionalDistribution;
-import eu.amidst.core.distribution.Multinomial;
-import eu.amidst.core.distribution.Multinomial_MultinomialParents;
-import eu.amidst.core.distribution.UnivariateDistribution;
+import eu.amidst.core.distribution.*;
 
 /**
  * Created by andresmasegosa on 12/11/14.
@@ -18,5 +15,28 @@ public class EF_DistributionBuilder {
         return null;
     }
 
+    public static EF_Normal newEFFromUnivariateDistribution(Normal dist){
+
+        EF_Normal ef_normal = new EF_Normal(dist.getVariable());
+        MomentParameters momentParameters = new MomentParameters(2);
+        momentParameters.set(EF_Normal.EXPECTED_MEAN,dist.getMean());
+        momentParameters.set(EF_Normal.EXPECTED_SQUARE,dist.getMean()*dist.getMean() + dist.getSd()*dist.getSd());
+        ef_normal.setMomentParameters(momentParameters);
+        return ef_normal;
+
+    }
+
+
+    public static Normal newNormalFromEF_NormalDistribution(EF_Normal ef_normal){
+
+        Normal normal = new Normal(ef_normal.getVariable());
+        double mean = ef_normal.getMomentParameters().get(EF_Normal.EXPECTED_MEAN);
+        double sigma = ef_normal.getMomentParameters().get(EF_Normal.EXPECTED_SQUARE) - mean*mean;
+
+        normal.setMean(mean);
+        normal.setSd(Math.sqrt(sigma));
+
+        return normal;
+    }
 
 }
