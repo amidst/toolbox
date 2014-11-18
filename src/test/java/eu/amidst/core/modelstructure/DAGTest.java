8931d857309a69b52eb49650ffa0668d2ea80629
HanenBorchani <Hanen@h74.net2.klient.slv.site.aau.dk>
2014-11-18 14:40:17 +0100

    testing dag

---
+++ src/test/java/eu/amidst/core/modelstructure/DAGTest.java
@@ -0,0 +1,76 @@
+package eu.amidst.core.modelstructure;
+
+import eu.amidst.core.database.filereaders.arffWekaReader.WekaDataFileReader;
+import eu.amidst.core.database.Attribute;
+import eu.amidst.core.database.Attributes;
+import eu.amidst.core.header.*;
+import java.util.*;
+import eu.amidst.core.database.filereaders.arffFileReader.*;
+import eu.amidst.core.database.filereaders.arffWekaReader.*;
+import eu.amidst.core.database.dynamics.readers.*;
+import eu.amidst.core.modelstructure.DAG;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by Hanen on 14/11/14.
+ */
+public class DAGTest {
+
+/* Very simple example to test the DAG class*/
+
+    WekaDataFileReader reader = new WekaDataFileReader("data/dataWeka/contact-lenses.arff");
+    StaticModelHeader modelheader = new StaticModelHeader(reader.getAttributes());
+    DAG dag = new DAG(modelheader);
+
+    @Test
+    public void testingDAG(){
+        List<Variable> variables =  dag.getModelHeader().getVariables();
+        Variable A = variables.get(0);
+        Variable B = variables.get(1);
+        Variable C = variables.get(2);
+        Variable D = variables.get(3);
+        Variable E = variables.get(4);
+
+        /* test cyclic dag */
+
+        dag.addParent(A,C);
+        dag.addParent(B,A);
+        dag.addParent(C,B);
+        dag.addParent(D,B);
+        dag.addParent(E,B);
+
+        assertTrue(dag.containCycles());
+
+
+        /*remove the cycle and test again */
+
+        dag.removeParent(A,C);
+        dag.addParent(C,A);
+
+        assertFalse(dag.containCycles());
+
+
+        /*test the parent set*/
+
+        assertEquals(2, dag.getParentSet(C).getNumberOfParents());
+        assertEquals(0, dag.getParentSet(A).getNumberOfParents());
+
+
+        /*test number of arcs*/
+
+        assertEquals(5, dag.getNumberOfArcs());
+
+        try {
+            dag.removeParent(C,A);
+        } catch (Exception e) {
+            fail("Should not throw Exception:" + e);
+        }
+        assertEquals(4, dag.getNumberOfArcs());
+
+        assertEquals(0, (new DAG(modelheader)).getNumberOfArcs());
+
+    }
+
+}
