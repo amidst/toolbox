b5f411bd056d7b5b9853cbd4335d74bb4f2c5595
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2014-11-20 13:19:41 +0100

    Update on exp. family

--- src/main/java/eu/amidst/core/exponentialfamily/SufficientStatistics.java
+++ src/main/java/eu/amidst/core/exponentialfamily/SufficientStatistics.java
@@ -1,11 +1,12 @@
 package eu.amidst.core.exponentialfamily;
 
+import eu.amidst.core.utils.ArrayVector;
 import eu.amidst.core.utils.Vector;
 
 /**
  * Created by andresmasegosa on 12/11/14.
  */
-public class SufficientStatistics extends Vector {
+public class SufficientStatistics extends ArrayVector {
 
     public SufficientStatistics(int size) {
         super(size);
