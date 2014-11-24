c4fb15370d552aed88291b8c85d9b5e2c23646d7
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2014-11-24 13:44:51 +0100

    Update

--- src/main/java/eu/amidst/examples/StaticBayesianNetworks.java
+++ src/main/java/eu/amidst/examples/StaticBayesianNetworks.java
@@ -8,10 +8,11 @@ import eu.amidst.core.database.filereaders.arffWekaReader.WekaDataFileReader;
 import eu.amidst.core.huginlink.ConverterToHugin;
 import eu.amidst.core.models.BayesianNetwork;
 import eu.amidst.core.models.DAG;
-import eu.amidst.core.variables.StaticVariables;
-import eu.amidst.core.variables.Variable;
+import eu.amidst.core.variables.*;
 
 /**
+ *
+ *
  * Created by andresmasegosa on 22/11/14.
  */
 public class StaticBayesianNetworks {
@@ -72,7 +73,58 @@ public class StaticBayesianNetworks {
 
     }
 
+    public static void staticBNWithHidden() throws Exception {
+        //**************************************** Synthetic data ******************************************************
+        DataOnDisk data = new StaticDataOnDiskFromFile(new WekaDataFileReader(new String("datasets/syntheticData.arff")));
+
+        //***************************************** Network structure **************************************************
+        //Create the structure by hand
+        StaticVariables variables = new StaticVariables(data.getAttributes());
+
+        Variable A = variables.getVariableByName("A");
+        Variable B = variables.getVariableByName("B");
+        Variable C = variables.getVariableByName("C");
+        Variable D = variables.getVariableByName("D");
+        Variable E = variables.getVariableByName("E");
+        Variable G = variables.getVariableByName("G");
+        Variable H = variables.getVariableByName("H");
+        Variable I = variables.getVariableByName("I");
+
+        VariableBuilder variableBuilder = new VariableBuilder();
+        variableBuilder.setName("HiddenVar");
+        variableBuilder.setObservable(false);
+        variableBuilder.setStateSpaceType(StateSpaceType.MULTINOMIAL);
+        variableBuilder.setDistributionType(DistType.MULTINOMIAL);
+
+        Variable hidden = variables.addHiddenVariable(variableBuilder);
+
+        //Example
+        DAG dag = new DAG(variables);
+
+        dag.getParentSet(A).addParent(hidden);
+        dag.getParentSet(B).addParent(hidden);
+        dag.getParentSet(C).addParent(hidden);
+        dag.getParentSet(D).addParent(hidden);
+        dag.getParentSet(E).addParent(hidden);
+        dag.getParentSet(G).addParent(hidden);
+        dag.getParentSet(H).addParent(hidden);
+        dag.getParentSet(I).addParent(hidden);
+
+        System.out.println(dag.toString());
+
+        BayesianNetwork bn = BayesianNetwork.newBayesianNetwork(dag);
+
+        System.out.println(bn.toString());
+
+        ConverterToHugin converterToHugin = new ConverterToHugin(bn);
+        converterToHugin.convertToHuginBN();
+        String outFile = new String("networks/huginStaticBNHiddenExample.net");
+        converterToHugin.getHuginNetwork().saveAsNet(new String(outFile));
+
+    }
+
     public static void main(String[] args) throws Exception {
         StaticBayesianNetworks.staticBNNoHidden();
+        StaticBayesianNetworks.staticBNWithHidden();
     }
 }
