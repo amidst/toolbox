84bcf0bd8b449a1e83c9124f5dc4ebc61547c90a
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2014-12-16 11:06:18 +0100

    Update

--- src/main/java/eu/amidst/core/utils/BayesianNetworkSampler.java
+++ src/main/java/eu/amidst/core/utils/BayesianNetworkSampler.java
@@ -1,32 +1,24 @@
 package eu.amidst.core.utils;
 
-import COM.hugin.HAPI.ExceptionHugin;
 import com.google.common.base.Stopwatch;
-import eu.amidst.core.database.*;
 import eu.amidst.core.models.BayesianNetwork;
 import eu.amidst.core.models.BayesianNetworkLoader;
 import eu.amidst.core.models.DAG;
-import eu.amidst.core.variables.*;
+import eu.amidst.core.variables.Assignment;
+import eu.amidst.core.variables.HashMapAssignment;
+import eu.amidst.core.variables.StaticVariables;
+import eu.amidst.core.variables.Variable;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
-//import org.openjdk.jmh.annotations.*;
 
 
 /**
@@ -49,24 +41,26 @@ public class BayesianNetworkSampler  {
         this.causalOrder=BayesianNetworkSampler.getCausalOrder(network.getDAG());
     }
 
-    /*
+
     public Stream<Assignment> getSampleStream(int nSamples) {
         if (parallelMode){
             LocalRandomGenerator randomGenerator = new LocalRandomGenerator(seed);
-            sampleStream = IntStream.range(0, nSamples).mapToObj(i -> sample(network, causalOrder, randomGenerator.current()));
+            sampleStream = IntStream.range(0, nSamples).parallel().mapToObj(i -> sample(network, causalOrder, randomGenerator.current()));
         }else{
             Random random  = new Random(seed);
             sampleStream =  IntStream.range(0, nSamples).mapToObj(e -> sample(network, causalOrder, random));
         }
-        return (parallelMode)? sampleStream.parallel() : sampleStream;
-    }*/
+        return sampleStream;
+    }
 
 
+    /*
     public Stream<Assignment> getSampleStream(int nSamples) {
         LocalRandomGenerator randomGenerator = new LocalRandomGenerator(seed);
-        sampleStream =  IntStream.range(0, nSamples).mapToObj(i -> sample(network, causalOrder, randomGenerator.current()));
+        //sampleStream =  IntStream.range(0, nSamples).mapToObj(i -> sample(network, causalOrder, randomGenerator.current()));
+        sampleStream =  IntStream.range(0, nSamples).mapToObj(i -> sample(network, causalOrder, new Random(i)));
         return (parallelMode)? sampleStream.parallel() : sampleStream;
-    }
+    }*/
 
     public List<Assignment> getSampleList(int nSamples){
         return this.getSampleStream(nSamples).collect(Collectors.toList());
@@ -117,6 +111,12 @@ public class BayesianNetworkSampler  {
 
     private static Assignment sample(BayesianNetwork network, List<Variable> causalOrder, Random random) {
 
+        try{
+            Thread.sleep(1000);
+        }catch(Exception e){
+
+        }
+
         HashMapAssignment assignment = new HashMapAssignment(network.getNumberOfVars());
         for (Variable var : causalOrder) {
             double sampledValue = network.getDistribution(var).getUnivariateDistribution(assignment).sample(random);
@@ -157,11 +157,15 @@ public class BayesianNetworkSampler  {
     public static void main(String[] args) throws Exception{
 
         Stopwatch watch = Stopwatch.createStarted();
+
         BayesianNetwork network = BayesianNetworkLoader.loadFromHugin("./networks/asia.net");
+
         BayesianNetworkSampler sampler = new BayesianNetworkSampler(network);
         sampler.setSeed(0);
         sampler.setParallelMode(true);
+
         sampler.sampleToAnARFFFile("./data/asisa-samples.arff", 10);
+
         System.out.println(watch.stop());
 
         for (Assignment assignment : sampler.getSampleIterator(2)){
