13a98c8ed2f953784b302887b2c59ec82d0f2742
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2015-01-07 16:15:52 +0100

    Update

--- src/main/java/eu/amidst/core/exponentialfamily/SufficientStatistics.java
+++ src/main/java/eu/amidst/core/exponentialfamily/SufficientStatistics.java
@@ -7,7 +7,8 @@ import eu.amidst.core.utils.Vector;
  * Created by andresmasegosa on 12/11/14.
  */
 public interface SufficientStatistics extends Vector {
-    public static SufficientStatistics sum(SufficientStatistics vec1, SufficientStatistics vec2){
-        return vec1;//(SufficientStatistics) Vector.sum(vec1,vec2);
+    public static SufficientStatistics sumSS(SufficientStatistics vec1, SufficientStatistics vec2){
+        vec1.sum(vec2);
+        return vec1;
     }
 }
