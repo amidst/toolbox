681fbd3893be33b39da3fcdc813bc82f8f7d1acc
Antonio Fernández <afalvarez@ual.es>
2015-01-12 20:17:40 +0100

    Converter from a DBN in Hugin into AMIDST format.

---
+++ src/test/java/eu/amidst/core/huginlink/DBNConverterToAmidstTest.java
@@ -0,0 +1,46 @@
+package eu.amidst.core.huginlink;
+
+import COM.hugin.HAPI.*;
+import COM.hugin.HAPI.Class;
+import eu.amidst.core.models.DynamicBayesianNetwork;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by afa on 12/1/15.
+ */
+public class DBNConverterToAmidstTest {
+
+    @Before
+    public void setUp() throws ExceptionHugin {
+
+        DynamicBayesianNetwork amidstDBN = DBNConverterToHuginTest.getAmidst_DBN_Example();
+        System.out.println("\nConverting the AMIDST Dynamic BN into Hugin format ...");
+        Class huginDBN = DBNConverterToHugin.convertToHugin(amidstDBN);
+        //The name of the DBN must be the same as the name of the .net file !!!
+        String nameModel = "huginDBNFromAMIDST";
+        huginDBN.setName(nameModel);
+        String outFile = new String("networks/"+nameModel+".net");
+        huginDBN.saveAsNet(outFile);
+        System.out.println("Hugin network saved in \"" + outFile + "\"" + ".");
+
+        System.out.println("\nConverting the Hugin Dynamic BN into AMIDST format ...");
+        amidstDBN = DBNConverterToAmidst.convertToAmidst(huginDBN);
+
+        System.out.println("\nConverting the AMIDST Dynamic BN into Hugin format ...");
+        huginDBN = DBNConverterToHugin.convertToHugin(amidstDBN);
+
+        nameModel = "huginDBNFromAMIDST2";
+        //The name of the DBN must be the same as the name of the .net file !!!
+        huginDBN.setName(nameModel);
+        outFile = new String("networks/"+nameModel+".net");
+        huginDBN.saveAsNet(outFile);
+        System.out.println("Hugin network saved in \"" + outFile + "\"" + ".");
+
+    }
+
+    @Test
+    public void testModels() throws ExceptionHugin {
+
+    }
+}
