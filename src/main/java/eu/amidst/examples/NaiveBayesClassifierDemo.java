be92a163686bd9ef97fb88b0223cbe33978ddbcb
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2015-01-15 15:28:07 +0100

    BUG fixed in ML in parallel mode.

---
+++ src/main/java/eu/amidst/examples/NaiveBayesClassifierDemo.java
@@ -0,0 +1,35 @@
+package eu.amidst.examples;
+
+import eu.amidst.core.database.DataBase;
+import eu.amidst.core.learning.NaiveBayesClassifier;
+import eu.amidst.core.models.BayesianNetwork;
+import eu.amidst.core.utils.BayesianNetworkGenerator;
+import eu.amidst.core.utils.BayesianNetworkSampler;
+
+import java.util.Random;
+
+/**
+ * Created by andresmasegosa on 15/01/15.
+ */
+public class NaiveBayesClassifierDemo {
+    public static void main(String[] args) {
+
+        BayesianNetworkGenerator.setNumberOfContinuousVars(0);
+        BayesianNetworkGenerator.setNumberOfDiscreteVars(50000);
+        BayesianNetworkGenerator.setNumberOfStates(10);
+        BayesianNetwork bn = BayesianNetworkGenerator.generateNaiveBayes(new Random(0),2);
+
+        int sampleSize = 100;
+        BayesianNetworkSampler sampler = new BayesianNetworkSampler(bn);
+        sampler.setParallelMode(true);
+        DataBase data =  sampler.sampleToDataBase(sampleSize);
+
+        NaiveBayesClassifier model = new NaiveBayesClassifier();
+        model.setClassVarID(data.getAttributes().getNumberOfAttributes() - 1);
+        model.setParallelMode(true);
+        model.learn(data);
+        BayesianNetwork nbClassifier = model.getBNModel();
+        System.out.println(nbClassifier.toString());
+
+    }
+}
