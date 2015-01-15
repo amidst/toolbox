ff855a18a8d7602a3d929a58857561271cae4b5e
anamf <ana@h359.eduroam-2b.wlan.aalborguniversitet.dk>
2015-01-15 14:47:03 +0100

    Bug fixed in ParallelTANDemo

--- maven_startup.sh
+++ maven_startup.sh
@@ -2,9 +2,9 @@
 
 # Example script for setting up Maven on Mac OS X. May need modifications.
 
-export M2_HOME=/Applications/apache-maven-3.2.5
+export M2_HOME=/Applications/apache-maven-3.2.3
 export M2=$M2_HOME/bin
 export MAVEN_OPTS="-Xms256m -Xmx512m"
 export PATH=$M2:$PATH
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_20.jdk/Contents/Home
 export PATH=${JAVA_HOME}/bin/:$PATH
