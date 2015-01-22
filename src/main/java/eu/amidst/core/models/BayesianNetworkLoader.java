7d046f0e6a02fb627130225b704e39cb24dd41b3
Antonio Fernández <afalvarez@ual.es>
2015-01-22 14:50:39 +0100

    Some advances in the Loaders and Writers

--- src/main/java/eu/amidst/core/models/BayesianNetworkLoader.java
+++ src/main/java/eu/amidst/core/models/BayesianNetworkLoader.java
@@ -14,12 +14,22 @@ import java.io.*;
  */
 public class BayesianNetworkLoader {
 
+
+    //TODO Move this method to the module huginLink
     public static BayesianNetwork loadFromHugin(String file) throws ExceptionHugin {
 
         ParseListener parseListener = new DefaultClassParseListener();
         Domain huginBN = new Domain (file, parseListener);
         BayesianNetwork amidstBN = ConverterToAMIDST.convertToAmidst(huginBN);
         return amidstBN;
+    }
+
+    public static BayesianNetwork loadFromFile(String fileName) throws IOException, ClassNotFoundException {
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName));
+        Object obj = ois.readObject();
+        ois.close();
+        return (BayesianNetwork)obj;
     }
 }
 
