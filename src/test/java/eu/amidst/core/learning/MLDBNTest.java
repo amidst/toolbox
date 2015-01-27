e40b317094cc6fbf95a17a123ab0b8f25e971bb5
Hanen <Hanen@h74.net2.klient.slv.site.aau.dk>
2015-01-27 16:10:27 +0100

    no message

--- src/test/java/eu/amidst/core/learning/MLDBNTest.java
+++ src/test/java/eu/amidst/core/learning/MLDBNTest.java
@@ -25,11 +25,14 @@ public class MLDBNTest {
     @Test
     public void testingMLforDBN() throws ExceptionHugin, IOException, ClassNotFoundException {
 
-        //Generate a dynamic Naive Bayes with only multinomials variables
-
+        //Generate a dynamic Naive Bayes with only Multinomial variables
         DynamicBayesianNetworkGenerator dbnGenerator = new DynamicBayesianNetworkGenerator();
+
+        //TODO Check for Gaussian variables too!
+
+        //Set the number of Discrete and Continuous variables, in this case we have 0 continuous vars
         dbnGenerator.setNumberOfContinuousVars(0);
-        dbnGenerator.setNumberOfDiscreteVars(2);
+        dbnGenerator.setNumberOfDiscreteVars(5);
         dbnGenerator.setNumberOfStates(2);
 
         DynamicBayesianNetwork dynamicNB = DynamicBayesianNetworkGenerator.generateDynamicNaiveBayes(new Random(0), 2);
@@ -43,7 +46,7 @@ public class MLDBNTest {
         sampler.setSeed(0);
         sampler.setParallelMode(true);
 
-        //Sample from the dynamic NB given as inputs both nSequences and sequenceLength
+        //Sample from the dynamic NB given as inputs both nSequences (= 10000) and sequenceLength (= 100)
 
         DataBase data = sampler.sampleToDataBase(10000,100);
 
@@ -53,24 +56,24 @@ public class MLDBNTest {
 
         //Parameter Learning
         MaximumLikelihood.setBatchSize(1000);
-        MaximumLikelihood.setParallelMode(false);
+        MaximumLikelihood.setParallelMode(true);
 
         DynamicBayesianNetwork bnet = MaximumLikelihood.learnDynamic(dynamicNB.getDynamicDAG(), data);
 
-        //Check if the probability distributions of each node
+        //Check if the probability distributions of each node over both time 0 and T
         for (Variable var : dynamicNB.getDynamicVariables()) {
-            System.out.println("\n------ Variable " + var.getName() + " ------");
+            System.out.println("\n---------- Variable " + var.getName() + " -----------");
             // time 0
-            System.out.println("\nTrue distribution:\n" + dynamicNB.getDistributionTime0(var));
-            System.out.println("\nLearned distribution:\n"+ bnet.getDistributionTime0(var));
-            //assertTrue(bnet.getDistributionTime0(var).equalDist(dynamicNB.getDistributionTime0(var), 0.05));
+            System.out.println("\nTrue distribution at time 0:\n" + dynamicNB.getDistributionTime0(var));
+            System.out.println("\nLearned distribution at time 0:\n"+ bnet.getDistributionTime0(var));
+            assertTrue(bnet.getDistributionTime0(var).equalDist(dynamicNB.getDistributionTime0(var), 0.05));
             // time T
-            System.out.println("\nTrue distribution:\n"+ dynamicNB.getDistributionTimeT(var));
-            System.out.println("\nLearned distribution:\n"+ bnet.getDistributionTimeT(var));
-            //assertTrue(bnet.getDistributionTimeT(var).equalDist(dynamicNB.getDistributionTimeT(var), 0.05));
+            System.out.println("\nTrue distribution at time T:\n"+ dynamicNB.getDistributionTimeT(var));
+            System.out.println("\nLearned distribution at time T:\n"+ bnet.getDistributionTimeT(var));
+            assertTrue(bnet.getDistributionTimeT(var).equalDist(dynamicNB.getDistributionTimeT(var), 0.05));
         }
 
-        //Or check directly if the true and learned networks are equals
+        //Or check directly if the true and learned dynamic networks are equals
         assertTrue(bnet.equalDBNs(dynamicNB,0.05));
     }
 
