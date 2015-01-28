766f86472010be6ca66fba73a86da06cd92553bf
Antonio Fernández <afalvarez@ual.es>
2015-01-28 13:30:48 +0100

    A new module called "huginlink" is created (depending on core). The hugin libraries must be located in the "huginlink" module and not in the core. Now module "core" does not have any hugin code.

--- src/main/java/eu/amidst/core/models/DynamicBayesianNetworkLoader.java
+++ src/main/java/eu/amidst/core/models/DynamicBayesianNetworkLoader.java
@@ -1,9 +1,5 @@
 package eu.amidst.core.models;
 
-import COM.hugin.HAPI.*;
-import COM.hugin.HAPI.Class;
-import eu.amidst.core.huginlink.DBNConverterToAmidst;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -12,15 +8,6 @@ import java.io.ObjectInputStream;
  * Created by Hanen on 15/01/15.
  */
 public class DynamicBayesianNetworkLoader {
-
-    //TODO Move this method to the module huginLink
-
-    public static DynamicBayesianNetwork loadDBNFromHugin(String file) throws ExceptionHugin {
-
-            Class huginDBN = new Class(new ClassCollection());
-            DynamicBayesianNetwork amidstDBN = DBNConverterToAmidst.convertToAmidst(huginDBN);
-            return amidstDBN;
-        }
 
     public static DynamicBayesianNetwork loadFromFile (String fileName) throws ClassNotFoundException, IOException {
 
