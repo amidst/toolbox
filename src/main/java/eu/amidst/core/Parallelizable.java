eb6403bfeeffd1c833f400400740d08ba0472bb0
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2015-01-30 14:44:30 +0100

    Inference Engine for BNs

---
+++ src/main/java/eu/amidst/core/Parallelizable.java
@@ -0,0 +1,13 @@
+package eu.amidst.core;
+
+/**
+ * Created by andresmasegosa on 30/01/15.
+ */
+public interface Parallelizable {
+
+    public static void setParallelMode(boolean parallelMode){}
+
+
+    public static boolean isParallelMode(){ return false;};
+
+}
