9662fc179f2691bd4cb8d30b7edbd76b5155936c
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2015-02-04 15:38:26 +0100

    Message Passing methods for EF_BaseDistribtuion. First Implementation.

--- src/main/java/eu/amidst/Main.java
+++ src/main/java/eu/amidst/Main.java
@@ -52,10 +52,50 @@ public class Main {
 package eu.amidst;
 
 
+import java.util.*;
+import java.util.function.IntConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.*;
+
 public class Main {
 
  public static void main(String[] args) throws Exception {
 
+
+     IntStream stream = IntStream.range(0,10);
+
+     OptionalInt optionalInt = stream.filter(i -> i >= 3).findFirst();
+
+     System.out.println(optionalInt.isPresent());
+     System.out.println(optionalInt.getAsInt());
+
+     //stream.forEach(System.out::println);
+
+     List<Integer> list = new ArrayList<>(Arrays.asList(1, 10, 3, 7, 5));
+     Stream<Integer> streamList = list.stream();
+     int a = streamList.filter(x -> {System.out.println("filtered"); return x > 5;}).findFirst().get();
+     System.out.println(a);
+
+     //stream.forEach(System.out::println);
+
+
+
+     Stream<String> words = Stream.of("Java", "Magazine", "is",
+             "the", "best");
+
+     Map<String, Long> letterToCount =
+             words.map(w -> w.split(""))
+                     .flatMap(Arrays::stream)
+                     .collect(Collectors.groupingBy(identity(), counting()));
+
+     System.out.println(letterToCount.toString());
+
+
+     System.out.println(letterToCount.toString());
  }
 
 }
