da2f8ccc2fbd4324a108e86a2c55eb95e7d77bde
Ana M. Martinez <anam.martinezf@gmail.com>
2015-02-19 16:05:20 +0100

    Option handling tested (not bullet proof though) for Parallel TAN.\n - Non-static classes should extend AmidstOptionsHandler \n - Static classes should implement the method in AmidstOptionsHandlerStatic\n - The main method should invoque loadOptions(), with or without arguments

--- src/main/java/eu/amidst/core/utils/OptionParser.java
+++ src/main/java/eu/amidst/core/utils/OptionParser.java
@@ -1,10 +1,8 @@
 package eu.amidst.core.utils;
 
-import org.apache.log4j.spi.OptionHandler;
 import org.eclipse.jetty.io.UncheckedIOException;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,7 +16,8 @@ public class OptionParser {
     private static final String subOptionSeparator = "--";
 
     private static HashMap<String,HashMap<String,String>> allOptions;
-    private static String confFileName = "./conf.txt";
+    private static String confFileNameDefault = "configurationFiles/conf.txt";
+    private static String confFileName = "";
     private static String[] commandLineOptions;
     /**
      * Contains all options (recursive one included) that are used for a particular execution
@@ -37,17 +36,23 @@ public class OptionParser {
     public static void setArgsOptions(String classNameID, String[] args) {
 
 
-        commandLineOptions = new String[args.length];
+        commandLineOptions = new String[args.length+1];
+        commandLineOptions[0] = subOptionSeparator+classNameID;
         int offset = 0;
         /*
          * Check -help and -confFileName (name of the class should be passed) and store args
          */
         for (int i = 0; i < args.length; i++) {
             if(args[i].equalsIgnoreCase("-help") || args[i].equalsIgnoreCase("--help")){
-                if(args[i++].equalsIgnoreCase("-r")) {
-                    System.out.println(getListOptionsRecursively(classNameID));
-                }else{
-                    System.out.println(getListOptions(classNameID));
+                 if(args.length == 1){
+                    String options = getListOptions(classNameID);
+                    options = options.replace("\\","\n");
+                    System.out.println(options);
+                }else if(args[++i].equalsIgnoreCase("-r")){
+                     //TODO: Parse this output to make it look/read good
+                    String options = getListOptionsRecursively(classNameID);
+                    options = options.replace("\\","\n");
+                    System.out.println(options);
                 }
                 System.exit(0);
             }
@@ -55,7 +60,7 @@ public class OptionParser {
                 setConfFileName(args[i++]);
                 offset = 2;
             }else{
-                commandLineOptions[i-offset] = args[i];
+                commandLineOptions[i+1-offset] = args[i];
             }
         }
         if(offset==2) {//Remove two last empty elements
@@ -75,40 +80,59 @@ public class OptionParser {
          * The first time it is called, it must put all options in a String (recursively) and print it at some point.
          */
         if(allOptions == null) {
-            loadFileOptions(classNameID);
+            loadFileOptions();
             loadCommandLineOptions(classNameID);
             loadDefaultOptions(defaultOptions); //Only puts if absent
             optionString = allOptions.toString();
         }else if(allOptions.get(classNameID) == null){
             loadDefaultOptions(defaultOptions); //Only puts if absent
-         }
+         }else if(allOptions.get(classNameID).get(optionName) == null){
+            loadDefaultOptions(defaultOptions); //Only puts if absent
+        }
         return allOptions.get(classNameID).get(optionName);
     }
 
 
-    public static void loadFileOptions(String classNameID){
+    public static void loadFileOptions(){
 
         allOptions = new HashMap<>();
-        Path pathFile = Paths.get(confFileName);
-        try {
-            Files.lines(pathFile)
-                    .filter(line -> !line.startsWith("%"))
-                    .forEach(line -> parseLine(line));
-        }catch (IOException ex){
-            throw new UncheckedIOException(ex);
+        if(confFileName.isEmpty()) {
+            Path pathFile = Paths.get(confFileNameDefault);
+            try {
+                Files.lines(pathFile)
+                        .filter(line -> !line.startsWith("%"))
+                        .forEach(line -> parseLine(line));
+            } catch (IOException ex) {
+                //Continue with default options
+            }
+        }else{
+            Path pathFile = Paths.get(confFileName);
+            try {
+                Files.lines(pathFile)
+                        .filter(line -> !line.startsWith("%"))
+                        .forEach(line -> parseLine(line));
+            } catch (IOException ex) {
+                throw new UncheckedIOException(ex);
+            }
         }
     }
 
     private static void parseLine(String line){
-        String[] splitLine = line.split(" ");
+        String[] splitLine = line.split("\\s+|\\s*,\\s*|\\s*=\\s*");
         HashMap<String,String> local = new HashMap<>();
 
-        for (int i = 1; i < splitLine.length-1; i++) {
-            local.put(splitLine[i], splitLine[i++]);
-        }
+        if(allOptions.get(splitLine[0])==null) {
+            for (int i = 1; i < splitLine.length - 1; i++) {
+                local.put(splitLine[i].trim(), splitLine[++i].trim());
+            }
 
-        if(!local.isEmpty())
-            allOptions.put(splitLine[0],local);
+            if (!local.isEmpty())
+                allOptions.put(splitLine[0].trim(), local);
+        }else{//When this method is called from AmidstOptionHandler.loadOptionsFromFile()
+            for (int i = 1; i < splitLine.length - 1; i++) {
+                allOptions.get(splitLine[0]).put(splitLine[i].trim(), splitLine[++i].trim());
+            }
+        }
     }
 
     public static void loadCommandLineOptions(String classNameID){
@@ -117,13 +141,17 @@ public class OptionParser {
         HashMap<String,String> local = new HashMap<>();
 
         for (int i = 0; i < commandLineOptions.length; i++) {
-            if(commandLineOptions[i].equals(subOptionSeparator)) {
-                allOptions.put(className,local);
-                className = commandLineOptions[i++];
+            if(commandLineOptions[i].startsWith(subOptionSeparator)) {
+                if(!local.isEmpty()) {
+                    allOptions.put(className, local);
+                }
+                className = commandLineOptions[i].replace(subOptionSeparator,"");
                 local = new HashMap<>();
             }
-            else{
-                local.put(commandLineOptions[i], commandLineOptions[i++]);
+            else if(allOptions.get(className) == null){
+                local.put(commandLineOptions[i].trim(), commandLineOptions[++i].trim());
+            }else{
+                allOptions.get(className).put(commandLineOptions[i].trim(), commandLineOptions[++i].trim());
             }
         }
         //In case the separator (for subOptions) has not been added at the end
@@ -134,7 +162,7 @@ public class OptionParser {
 
     public static void loadDefaultOptions(String defaultOptions){
 
-        String[] options = defaultOptions.split(" |\\\\");
+        String[] options = defaultOptions.split(",\\\\|\\s*,\\s*|\\\\");
         String className = options[0];
 
         HashMap<String,String> local = new HashMap<>();
@@ -142,31 +170,34 @@ public class OptionParser {
         for (int i = 1; i < options.length; i++) {
             if(options[i].equals("\t")){
                 allOptions.putIfAbsent(className, local);
-                className = options[i++];
+                className = options[++i].trim();
                 local = new HashMap<>();
             }else if(allOptions.get(className) == null){
-                local.put(options[i], options[i++]);
+                local.put(options[i].trim(), options[++i].trim());
             }
             else{
-                allOptions.get(className).putIfAbsent(options[i], options[i++]);
+                allOptions.get(className).putIfAbsent(options[i].trim(), options[++i].trim());
             }
             i++; //Skip the description
+        }
+        if(!local.isEmpty()){
+            allOptions.put(className,local);
         }
     }
 
     public static String getListOptions(String classNameID){
         try {
-            return ((AmidstOptionsHandler) Class.forName(classNameID).getConstructor(String.class).newInstance()).listOptions();
+            return ((AmidstOptionsHandler) Class.forName(classNameID).newInstance()).listOptions();
         }catch (Exception e){
-            throw new IllegalArgumentException("The class" +classNameID+ "does not exist");
+            throw new IllegalArgumentException("The class " + classNameID + " does not exist");
         }
     }
 
     public static String getListOptionsRecursively(String classNameID){
         try {
-            return ((AmidstOptionsHandler) Class.forName(classNameID).getConstructor(String.class).newInstance()).listOptionsRecursively();
+            return ((AmidstOptionsHandler) Class.forName(classNameID).newInstance()).listOptionsRecursively();
         }catch (Exception e){
-            throw new IllegalArgumentException("The class" + classNameID+ "does not exist");
+            throw new IllegalArgumentException(e);
         }
     }
 
