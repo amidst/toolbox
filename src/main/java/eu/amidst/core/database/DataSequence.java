9ea3d80cfb687a82bb6494a56c1a13bbb129ad42
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2015-02-24 11:06:17 +0100

    Update fixing compilation errors

--- src/main/java/eu/amidst/core/database/DataSequence.java
+++
@@ -1,8 +0,0 @@
-package eu.amidst.core.database;
-
-/**
- * Created by andresmasegosa on 19/02/15.
- */
-public interface DataSequence extends DataBase<DynamicDataInstance>{
-    public int getSequeneID();
-}
