83a8be507500981becce4f83d45c22cb3bba9ab5
anamf <ana@h55.aau1x-2.klient.slv.site.aau.dk>
2015-02-26 16:03:56 +0100

    Update

--- src/main/java/eu/amidst/core/variables/DistType.java
+++
@@ -1,9 +0,0 @@
-package eu.amidst.core.variables;
-
-/**
- * Created by Hanen on 05/11/14.
- */
-
-public enum DistType {MULTINOMIAL, NORMAL, MULTINOMIAL_LOGISTIC, INDICATOR, INV_GAMMA, DIRICHLET;
-
-}
