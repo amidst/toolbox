6ad72f80ad328ed47fca0769414e6dadaa17b9ca
anamf <ana@h445.aau1x-2.klient.slv.site.aau.dk>
2015-03-04 15:36:00 +0100

    Update Bayesian Learning (static)

--- src/main/java/eu/amidst/core/inference/InferenceEngineForDBN.java
+++ src/main/java/eu/amidst/core/inference/InferenceEngineForDBN.java
@@ -35,7 +35,7 @@ public final class InferenceEngineForDBN {
     }
 
     public static DynamicBayesianNetwork getModel(){
-        return inferenceAlgorithmForDBN.getModel();
+        return inferenceAlgorithmForDBN.getOriginalModel();
     }
 
     public static void addDynamicEvidence(DynamicAssignment assignment){
