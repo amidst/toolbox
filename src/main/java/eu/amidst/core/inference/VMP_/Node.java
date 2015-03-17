6f2924ef5f48f2fa27ca4327b803728b67fcc14c
Ana M. Martinez <anam.martinezf@gmail.com>
2015-03-17 16:16:02 +0100

    BayesianVMPTest updated

--- src/main/java/eu/amidst/core/inference/VMP_/Node.java
+++ src/main/java/eu/amidst/core/inference/VMP_/Node.java
@@ -223,7 +223,7 @@ public class Node {
             elbo=0;
         }
 
-        if (elbo>0.01 && !this.isObserved()) {
+        if (elbo>0.1 && !this.isObserved()) {
             throw new IllegalStateException("NUMERICAL ERROR!!!!!!!!: " + this.getMainVariable().getName() + ", " +  elbo + ", " + expectedNatural.sum());
         }
 
