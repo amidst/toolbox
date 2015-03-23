d40a07db950c6f980685a805dd3ac6d7bda3dc64
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2015-03-23 08:55:11 +0100

    Maximum likelihood with fading factor

--- src/test/java/eu/amidst/core/learning/MLMultinomialsNormalsTest.java
+++ src/test/java/eu/amidst/core/learning/MLMultinomialsNormalsTest.java
@@ -2,7 +2,6 @@ package eu.amidst.core.learning;
 
 import eu.amidst.core.datastream.DataInstance;
 import eu.amidst.core.datastream.DataStream;
-import eu.amidst.core.learning.dynamic.MaximumLikelihoodForBN;
 import eu.amidst.core.models.BayesianNetwork;
 import eu.amidst.core.io.BayesianNetworkLoader;
 import eu.amidst.core.utils.BayesianNetworkSampler;
