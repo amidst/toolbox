9ec6d0b5b7da6247c81a29ffdae168b368688b27
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2015-04-14 10:25:06 +0200

    Udpdate

--- src/main/java/eu/amidst/ida2015/GlobalHiddenConceptDrift.java
+++ src/main/java/eu/amidst/ida2015/GlobalHiddenConceptDrift.java
@@ -4,6 +4,7 @@ import eu.amidst.core.datastream.DataInstance;
 import eu.amidst.core.datastream.DataOnMemory;
 import eu.amidst.core.datastream.DataStream;
 import eu.amidst.core.distribution.ConditionalDistribution;
+import eu.amidst.core.distribution.Multinomial;
 import eu.amidst.core.distribution.Normal;
 import eu.amidst.core.distribution.Normal_MultinomialNormalParents;
 import eu.amidst.core.learning.StreamingVariationalBayesVMP;
@@ -19,6 +20,46 @@ import java.util.Random;
  */
 public class GlobalHiddenConceptDrift {
 
+    private static double getRealMean(BayesianNetwork bn){
+
+        Variable classVariable = bn.getStaticVariables().getVariableByName("ClassVar");
+        Variable gaussianVar0 = bn.getStaticVariables().getVariableByName("GaussianVar0");
+
+        Multinomial distClass = bn.getDistribution(classVariable);
+        Normal_MultinomialNormalParents distGV0 = bn.getDistribution(gaussianVar0);
+
+        double inter0 = distGV0.getNormal_NormalParentsDistribution(0).getIntercept();
+        double inter1 = distGV0.getNormal_NormalParentsDistribution(1).getIntercept();
+
+        double mean = distClass.getProbabilityOfState(0)*inter0;
+        mean+= distClass.getProbabilityOfState(1)*inter1;
+
+        return mean;
+    }
+
+    private static double getLearntMean(BayesianNetwork bn, double globalMean){
+
+        Variable globalHidden = bn.getStaticVariables().getVariableByName("Global");
+        Variable classVariable = bn.getStaticVariables().getVariableByName("ClassVar");
+        Variable gaussianVar0 = bn.getStaticVariables().getVariableByName("GaussianVar0");
+
+
+        Normal distGlobal = bn.getDistribution(globalHidden);
+        Multinomial distClass = bn.getDistribution(classVariable);
+        Normal_MultinomialNormalParents distGV0 = bn.getDistribution(gaussianVar0);
+
+        double inter0 = distGV0.getNormal_NormalParentsDistribution(0).getIntercept();
+        double inter1 = distGV0.getNormal_NormalParentsDistribution(1).getIntercept();
+
+        double[] coef0 = distGV0.getNormal_NormalParentsDistribution(0).getCoeffParents();
+        double[] coef1 = distGV0.getNormal_NormalParentsDistribution(1).getCoeffParents();
+
+        double mean = distClass.getProbabilityOfState(0)*(inter0 + coef0[0]*globalMean);
+        mean+= distClass.getProbabilityOfState(1)*(inter1 + coef1[0]*globalMean);
+
+        return mean;
+    }
+
     public static void main(String[] args) {
 
         BayesianNetworkGenerator.setNumberOfContinuousVars(1);
@@ -29,6 +70,7 @@ public class GlobalHiddenConceptDrift {
 
         Variable globalHidden = naiveBayes.getStaticVariables().getVariableByName("Global");
         Variable classVariable = naiveBayes.getStaticVariables().getVariableByName("ClassVar");
+        Variable gaussianVar0 = naiveBayes.getStaticVariables().getVariableByName("GaussianVar0");
 
         for (ConditionalDistribution dist : naiveBayes.getConditionalDistributions()){
             if (dist.getVariable().equals(classVariable) || dist.getVariable().equals(globalHidden))
@@ -44,6 +86,7 @@ public class GlobalHiddenConceptDrift {
         sampler.setHiddenVar(globalHidden);
         DataStream<DataInstance> data = sampler.sampleToDataBase(1000);
         int windowSize = 100;
+        int count = windowSize;
 
         StreamingVariationalBayesVMP svb = new StreamingVariationalBayesVMP();
         svb.setPlateuStructure(new PlateuGlobalHiddenConceptDrift(globalHidden));
@@ -61,14 +104,17 @@ public class GlobalHiddenConceptDrift {
             //System.out.println("****************");
             //System.out.println(learntBN.toString());
             Normal normal = svb.getPlateuStructure().getEFVariablePosterior(globalHidden,0).toUnivariateDistribution();
-            System.out.println("Global Hidden: " + normal.getMean() +", " + normal.getVariance());
+            //System.out.println("Global Hidden: " + normal.getMean() +", " + normal.getVariance());
             //System.out.println("****************");
 
+            System.out.println(count + "\t" + normal.getMean() +"\t" + getLearntMean(learntBN, normal.getMean()) + "\t" + getRealMean(naiveBayes));
+
+            count+=windowSize;
         }
 
 
         for (int K=1; K<10; K++) {
-            System.out.println("******************************** CONCEPT DRIFT ********************************");
+            //System.out.println("******************************** CONCEPT DRIFT ********************************");
             naiveBayes.randomInitialization(new Random(K));
 
             for (ConditionalDistribution dist : naiveBayes.getConditionalDistributions()) {
@@ -79,7 +125,7 @@ public class GlobalHiddenConceptDrift {
                 newdist.getNormal_NormalParentsDistribution(1).setCoeffParents(new double[]{0.0});
             }
 
-            System.out.println(naiveBayes.toString());
+            //System.out.println(naiveBayes.toString());
 
             sampler = new BayesianNetworkSampler(naiveBayes);
             sampler.setHiddenVar(globalHidden);
@@ -93,9 +139,12 @@ public class GlobalHiddenConceptDrift {
                 //System.out.println("****************");
                 //System.out.println(learntBN.toString());
                 Normal normal = svb.getPlateuStructure().getEFVariablePosterior(globalHidden, 0).toUnivariateDistribution();
-                System.out.println("Global Hidden: " + normal.getMean() + ", " + normal.getVariance());
+                //System.out.println("Global Hidden: " + normal.getMean() + ", " + normal.getVariance());
                 //System.out.println("****************");
 
+                System.out.println(count + "\t" + normal.getMean() +"\t" + getLearntMean(learntBN, normal.getMean()) + "\t" + getRealMean(naiveBayes));
+
+                count+=windowSize;
             }
         }
     }
