6a5aa4abf2ad69d48ab94bdb01cb43e47df2082e
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2015-07-01 00:44:02 +0200

    Update

--- core-static/src/main/java/eu/amidst/core/inference/messagepassing/MessagePassingAlgorithm.java
+++ core-static/src/main/java/eu/amidst/core/inference/messagepassing/MessagePassingAlgorithm.java
@@ -43,7 +43,7 @@ public abstract class MessagePassingAlgorithm<E extends Vector> implements Infer
     protected double probOfEvidence = Double.NaN;
     protected Random random = new Random(0);
     protected int seed=0;
-    protected int maxIter = 1000;
+    protected int maxIter = 100;
     protected double threshold = 0.0001;
     protected boolean output = false;
     protected int nIter = 0;
