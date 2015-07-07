0c3fbc9050d5a52118368deef1ec35b4641920a5
dario <drl012@ual.es>
2015-07-07 13:52:49 +0200

    Some updates in Importance Sampling and preliminary MAP

---
+++ core-static/src/test/java/eu/amidst/core/inference/ImportanceSamplingExperiments.java
@@ -0,0 +1,210 @@
+package eu.amidst.core.inference;
+
+
+import eu.amidst.core.inference.messagepassing.VMP;
+import eu.amidst.core.io.BayesianNetworkLoader;
+import eu.amidst.core.models.BayesianNetwork;
+import eu.amidst.core.variables.Assignment;
+import eu.amidst.core.variables.HashMapAssignment;
+import eu.amidst.core.variables.Variable;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Random;
+
+/**
+ * Created by dario on 15/05/15.
+ */
+public class ImportanceSamplingExperiments {
+
+    private static Assignment randomEvidence(long seed, double evidenceRatio, BayesianNetwork bn, Variable varInterest) throws UnsupportedOperationException {
+
+        if (evidenceRatio<=0 || evidenceRatio>=1) {
+            throw new UnsupportedOperationException("Error: invalid ratio");
+        }
+
+        int numVariables = bn.getStaticVariables().getNumberOfVars();
+
+        Random random=new Random(seed); //1823716125
+        int numVarEvidence = (int) Math.ceil(numVariables*evidenceRatio); // Evidence on 20% of variables
+        //numVarEvidence = 0;
+        //List<Variable> varEvidence = new ArrayList<>(numVarEvidence);
+        double [] evidence = new double[numVarEvidence];
+        Variable aux;
+        HashMapAssignment assignment = new HashMapAssignment(2);
+
+        int[] indexesEvidence = new int[numVarEvidence+1];
+        indexesEvidence[0]=varInterest.getVarID();
+        //System.out.println(variable.getVarID());
+
+        System.out.println("Evidence:");
+        for( int k=0; k<numVarEvidence; k++ ) {
+            int varIndex=-1;
+            do {
+                varIndex = random.nextInt( bn.getNumberOfVars() );
+                //System.out.println(varIndex);
+                aux = bn.getStaticVariables().getVariableById(varIndex);
+
+                double thisEvidence;
+                if (aux.isMultinomial()) {
+                    thisEvidence = random.nextInt( aux.getNumberOfStates() );
+                }
+                else {
+                    thisEvidence = random.nextGaussian();
+                }
+                evidence[k] = thisEvidence;
+
+            } while ( ArrayUtils.contains(indexesEvidence, varIndex) );
+
+            indexesEvidence[k+1]=varIndex;
+            //System.out.println(Arrays.toString(indexesEvidence));
+            System.out.println("Variable " + aux.getName() + " = " + evidence[k]);
+
+            assignment.setValue(aux,evidence[k]);
+        }
+        System.out.println();
+        return assignment;
+    }
+
+
+    /**
+     * The class constructor.
+     * @param args Array of options: "filename variable a b N useVMP" if variable is continuous or "filename variable w N useVMP" for discrete
+     */
+    public static void main(String[] args) throws Exception {
+
+        //System.out.println("CONTINUOUS VARIABLE");
+        boolean discrete=false;
+
+        String filename=""; //Filename with the Bayesian Network
+        String varname=""; // Variable of interest in the BN
+        double a=0; // Lower endpoint of the interval
+        double b=0; // Upper endpoint of the interval
+        int N=0; // Sample size
+        boolean useVMP=false; // Boolean indicating whether use VMP or not
+
+
+
+
+        // FOR A CONTINUOUS VARIABLE OF INTEREST
+        if (args.length==6) {
+
+            filename = args[0]; //Filename with the Bayesian Network
+            varname = args[1]; // Variable of interest in the BN
+            String aa = args[2]; // Lower endpoint of the interval
+            String bb = args[3]; // Upper endpoint of the interval
+            String NN = args[4]; // Sample size
+            String useVMParg = args[5]; // Boolean indicating whether use VMP or not
+
+            try {
+                a = Double.parseDouble(aa);
+                b = Double.parseDouble(bb);
+                N = Integer.parseInt(NN);
+                useVMP = Boolean.parseBoolean(useVMParg);
+            }
+            catch (NumberFormatException e) {
+                System.out.println(e.toString());
+            }
+
+        }
+        // FOR A DISCRETE VARIABLE OF INTEREST
+        else if (args.length==5) {
+            //System.out.println("DISCRETE VARIABLE");
+            discrete=true;
+            System.out.println("Not available yet");
+            System.exit(1);
+        }
+        else if (args.length==0) {
+            filename="networks/Bayesian10Vars15Links.bn"; //Filename with the Bayesian Network
+            //filename="networks/Bayesian2Vars1Link.bn";
+            varname ="GaussianVar1"; // Variable of interest in the BN
+            a = 0; // Lower endpoint of the interval
+            b = 1; // Upper endpoint of the interval
+            N = 10000; // Sample size
+            useVMP = false; // Boolean indicating whether use VMP or not
+        }
+        else {
+            System.out.println("Invalid number of arguments. See comments in main");
+            System.exit(1);
+        }
+
+        BayesianNetwork bn;
+
+        VMP vmp = new VMP();
+
+
+        ImportanceSampling importanceSampling = new ImportanceSampling();
+
+
+        try {
+
+            bn = BayesianNetworkLoader.loadFromFile(filename);
+            System.out.println(bn.toString());
+            Variable varInterest = bn.getStaticVariables().getVariableByName(varname);
+
+
+            vmp.setModel(bn);
+            vmp.runInference();
+
+
+            importanceSampling.setModel(bn);
+            //importanceSampling.setSamplingModel(vmp.getSamplingModel());
+            importanceSampling.setSamplingModel(bn);
+            importanceSampling.setParallelMode(true);
+            importanceSampling.setSampleSize(N);
+
+
+            // Including evidence:
+            Assignment assignment = randomEvidence(1823716125,0.2, bn, varInterest);
+            importanceSampling.setEvidence(assignment);
+
+
+            importanceSampling.setSamplingModel(vmp.getSamplingModel());
+            importanceSampling.runInference(vmp);
+            System.out.println("Posterior of " + varInterest.getName() + "  (IS w. Evidence VMP) :" + importanceSampling.getPosterior(varInterest).toString());
+
+
+
+            importanceSampling.setSamplingModel(bn);
+            importanceSampling.runInference();
+            System.out.println("Posterior of " + varInterest.getName() + "  (IS w. Evidence) :" + importanceSampling.getPosterior(varInterest).toString());
+
+            System.out.println("Posterior of " + varInterest.getName() + " (VMP) :" + vmp.getPosterior(varInterest).toString());
+
+            System.out.println();
+
+
+            System.out.println("Variable of interest: " + varInterest.getName());
+            System.out.println();
+
+            a = 0.88; // Lower endpoint of the interval
+            b = 10000; // Upper endpoint of the interval
+
+            double result = importanceSampling.runQuery(varInterest, a, b);
+            System.out.println("Query: P(" + Double.toString(a) + " < " + varInterest.getName() + " < " + Double.toString(b) + ")");
+            System.out.println("Probability result: " + result);
+
+            /*
+            System.out.println();
+
+
+            varname = "DiscreteVar2";
+            System.out.println();
+            Variable discreteVarInterest = bn.getStaticVariables().getVariableByName(varname);
+            System.out.println("Variable of interest: " + discreteVarInterest.getName());
+
+            importanceSampling.runInference();
+
+            int w=1; // Value of interest
+            double result2 = importanceSampling.runQuery(discreteVarInterest, w);
+            System.out.println("Query: P(" + discreteVarInterest.getName() + " = " + Integer.toString(w) + ")");
+            System.out.println("Probability result: " + result2);*/
+
+
+        }
+        catch (Exception e) {
+            System.out.println("Error loading Bayesian Network from file");
+            System.out.println(e.toString());
+        }
+
+    }
+}
---
+++ core-static/src/test/java/eu/amidst/core/inference/MAPInferenceExperiments.java
@@ -0,0 +1,179 @@
+package eu.amidst.core.inference;
+
+
+import eu.amidst.core.inference.MAPInference;
+import eu.amidst.core.io.BayesianNetworkLoader;
+import eu.amidst.core.models.BayesianNetwork;
+import eu.amidst.core.utils.BayesianNetworkGenerator;
+import eu.amidst.core.utils.Utils;
+import eu.amidst.core.variables.Assignment;
+import eu.amidst.core.variables.HashMapAssignment;
+import eu.amidst.core.variables.Variable;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by dario on 01/06/15.
+ */
+public class MAPInferenceExperiments {
+
+    private static Assignment randomEvidence(long seed, double evidenceRatio, BayesianNetwork bn) throws UnsupportedOperationException {
+
+        if (evidenceRatio<=0 || evidenceRatio>=1) {
+            throw new UnsupportedOperationException("Error: invalid ratio");
+        }
+
+        int numVariables = bn.getStaticVariables().getNumberOfVars();
+
+        Random random=new Random(seed); //1823716125
+        int numVarEvidence = (int) Math.ceil(numVariables*evidenceRatio); // Evidence on 20% of variables
+        //numVarEvidence = 0;
+        //List<Variable> varEvidence = new ArrayList<>(numVarEvidence);
+        double [] evidence = new double[numVarEvidence];
+        Variable aux;
+        HashMapAssignment assignment = new HashMapAssignment(numVarEvidence);
+
+        int[] indexesEvidence = new int[numVarEvidence];
+        //indexesEvidence[0]=varInterest.getVarID();
+        //System.out.println(variable.getVarID());
+
+        System.out.println("Evidence:");
+        for( int k=0; k<numVarEvidence; k++ ) {
+            int varIndex=-1;
+            do {
+                varIndex = random.nextInt( bn.getNumberOfVars() );
+                //System.out.println(varIndex);
+                aux = bn.getStaticVariables().getVariableById(varIndex);
+
+                double thisEvidence;
+                if (aux.isMultinomial()) {
+                    thisEvidence = random.nextInt( aux.getNumberOfStates() );
+                }
+                else {
+                    thisEvidence = random.nextGaussian();
+                }
+                evidence[k] = thisEvidence;
+
+            } while (ArrayUtils.contains(indexesEvidence, varIndex) );
+
+            indexesEvidence[k]=varIndex;
+            //System.out.println(Arrays.toString(indexesEvidence));
+            System.out.println("Variable " + aux.getName() + " = " + evidence[k]);
+
+            assignment.setValue(aux,evidence[k]);
+        }
+        System.out.println();
+        return assignment;
+    }
+
+
+    /**
+     * The class constructor.
+     * @param args Array of options: "filename variable a b N useVMP" if variable is continuous or "filename variable w N useVMP" for discrete
+     */
+    public static void main(String[] args) throws Exception {
+
+        int seedNetwork = 61236719 + 123;
+
+        String filename=""; //Filename with the Bayesian Network
+
+        int nDiscrete = 10;
+        int nStates = 2;
+        int nContin = 10;
+        int nLinks = (int)Math.round(1.25*(nDiscrete+nContin));
+
+        filename = "networks/randomlyGeneratedBN.bn";
+        BayesianNetworkGenerator.generateBNtoFile(nDiscrete, nStates, nContin, nLinks, seedNetwork, filename);
+
+        int seed = seedNetwork + 2315;
+
+        BayesianNetwork bn = BayesianNetworkLoader.loadFromFile(filename);
+        System.out.println(bn.getDAG());
+
+        System.out.println(bn.toString());
+
+
+
+        MAPInference mapInference = new MAPInference();
+        mapInference.setModel(bn);
+        mapInference.setParallelMode(true);
+        mapInference.setSampleSize(1);
+
+        List<Variable> causalOrder = Utils.getCausalOrder(mapInference.getOriginalModel().getDAG());
+
+        System.out.println("CausalOrder: " + Arrays.toString(Utils.getCausalOrder(mapInference.getOriginalModel().getDAG()).stream().map(Variable::getName).toArray()));
+        System.out.println();
+
+        // Including evidence:
+        double observedVariablesRate = 0.1;
+        Assignment evidence = randomEvidence(seed, observedVariablesRate, bn);
+
+
+
+        mapInference.setEvidence(evidence);
+
+        long timeStart = System.nanoTime();
+        mapInference.runInference(1);
+
+        Assignment mapEstimate1 = mapInference.getMAPestimate();
+        List<Variable> modelVariables = mapInference.getOriginalModel().getStaticVariables().getListOfVariables();
+        System.out.println("MAP estimate: " + mapEstimate1.toString()); //toString(modelVariables);
+        System.out.println("with probability: " + Math.exp(mapInference.getOriginalModel().getLogProbabiltyOf(mapEstimate1)));
+        long timeStop = System.nanoTime();
+        double execTime = (double) (timeStop - timeStart) / 1000000000.0;
+        System.out.println("computed in: " + Double.toString(execTime) + " seconds");
+        //System.out.println(.toString(mapInference.getOriginalModel().getStaticVariables().iterator().));
+
+
+
+
+        // MAP INFERENCE WITH A BIG SAMPLE TO CHECK
+        mapInference.setSampleSize(50000);
+        timeStart = System.nanoTime();
+        mapInference.runInference(-1);
+
+        Assignment mapEstimate2 = mapInference.getMAPestimate();
+        modelVariables = mapInference.getOriginalModel().getStaticVariables().getListOfVariables();
+        System.out.println("MAP estimate (huge sample): " + mapEstimate2.toString());
+        System.out.println("with probability: " + Math.exp(mapInference.getOriginalModel().getLogProbabiltyOf(mapEstimate2)));
+        timeStop = System.nanoTime();
+        execTime = (double) (timeStop - timeStart) / 1000000000.0;
+        System.out.println("computed in: " + Double.toString(execTime) + " seconds");
+
+
+
+        // DETERMINISTIC SEQUENTIAL SEARCH ON DISCRETE VARIABLES
+        timeStart = System.nanoTime();
+        mapInference.runInference(-2);
+
+        Assignment mapEstimate3 = mapInference.getMAPestimate();
+        modelVariables = mapInference.getOriginalModel().getStaticVariables().getListOfVariables();
+        System.out.println("MAP estimate (sequential): " + mapEstimate3.toString());
+        System.out.println("with probability: " + Math.exp(mapInference.getOriginalModel().getLogProbabiltyOf(mapEstimate3)));
+        timeStop = System.nanoTime();
+        execTime = (double) (timeStop - timeStart) / 1000000000.0;
+        System.out.println("computed in: " + Double.toString(execTime) + " seconds");
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        mapInference.changeCausalOrder(bn,evidence);
+
+        /*// AD-HOC MAP
+        mapEstimate.setValue(bn.getStaticVariables().getVariableByName("GaussianVar0"),-0.11819702417804305);
+        mapEstimate.setValue(bn.getStaticVariables().getVariableByName("GaussianVar1"),-1.706);
+        mapEstimate.setValue(bn.getStaticVariables().getVariableByName("GaussianVar2"),4.95);
+        mapEstimate.setValue(bn.getStaticVariables().getVariableByName("GaussianVar3"),14.33);
+        mapEstimate.setValue(bn.getStaticVariables().getVariableByName("GaussianVar4"),11.355);
+
+        modelVariables = mapInference.getOriginalModel().getStaticVariables().getListOfVariables();
+        System.out.println("Other estimate: " + mapEstimate.toString(modelVariables));
+        System.out.println("with probability: " + Math.exp(mapInference.getOriginalModel().getLogProbabiltyOf(mapEstimate)));
+        */
+    }
+}
