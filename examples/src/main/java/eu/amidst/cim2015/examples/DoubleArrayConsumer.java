6947f3997b9e8e5b2922b1d3139524e6e509266a
anamf <anam.martinezf@gmail.com>
2015-07-08 13:09:27 +0200

    Parallel k-Means up and running - Not thoroughly testes though

--- examples/src/main/java/eu/amidst/cim2015/examples/DoubleArrayConsumer.java
+++
@@ -1,9 +0,0 @@
-package eu.amidst.cim2015.examples;
-
-/**
- * Created by ana@cs.aau.dk on 03/07/15.
- */
-@FunctionalInterface
-public interface DoubleArrayConsumer {
-    void accept(double[] value);
-}
