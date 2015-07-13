ddd38fa183dbbd09c693f17d71323fc8d4915031
dario <drl012@ual.es>
2015-07-13 16:26:45 +0200

    Improved "outputString" method

--- core-static/src/main/java/eu/amidst/core/variables/Assignment.java
+++ core-static/src/main/java/eu/amidst/core/variables/Assignment.java
@@ -69,8 +69,10 @@ public interface Assignment {
     default String outputString(List<Variable> vars){
         StringBuilder builder = new StringBuilder(vars.size()*2);
         builder.append("{");
-        vars.stream().limit(vars.size()-1).forEach(var -> builder.append( var.getName()+ " = "+ (var.isMultinomial() ? (int)this.getValue(var) : String.format("%1$,.3f", this.getValue(var)) ) + ", "));
-        builder.append(vars.get(vars.size()-1).getName()+ " = "+ (vars.get(vars.size()-1).isMultinomial() ? (int)this.getValue(vars.get(vars.size()-1)) : String.format("%1$,.3f", this.getValue(vars.get(vars.size()-1))) ));
+        vars.stream().limit(vars.size() - 1).filter(var -> !Double.isNaN(this.getValue(var))).forEach(var -> builder.append(var.getName() + " = " + (var.isMultinomial() ? (int) this.getValue(var) : String.format("%1$,.3f", this.getValue(var))) + ", "));
+        if(!Double.isNaN(this.getValue(vars.get(vars.size()-1)))) {
+            builder.append(vars.get(vars.size() - 1).getName() + " = " + (vars.get(vars.size() - 1).isMultinomial() ? (int) this.getValue(vars.get(vars.size() - 1)) : String.format("%1$,.3f", this.getValue(vars.get(vars.size() - 1)))));
+        }
         builder.append("}");
         return builder.toString();
     }
