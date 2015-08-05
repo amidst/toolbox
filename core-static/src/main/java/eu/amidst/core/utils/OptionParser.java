e240b13968c505177019c8dabed572a9b0b25af6
Hanen Borchani <hanene.borchani@gmail.com>
2015-08-05 16:28:47 +0200

    update

--- core-static/src/main/java/eu/amidst/core/utils/OptionParser.java
+++ core-static/src/main/java/eu/amidst/core/utils/OptionParser.java
@@ -16,41 +16,59 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 
 /**
- * Created by ana@cs.aau.dk on 17/02/15.
+ * This class handles util methods related for parsing the options or parameters.
  */
 public class OptionParser {
 
+    /** Represents a sub option separator. */
     private static final String subOptionSeparator = "--";
 
+    /** Represents a {@code HashMap} including all the options. */
     private static HashMap<String,HashMap<String,String>> allOptions;
+
+    /** Represents the default configuration file name. */
     private static String confFileNameDefault = "configurationFiles/conf.txt";
+
+    /** Represents the configuration file name. */
     private static String confFileName = "";
+
+    /** Represents an {@code array} of {@code String} including the command line options. */
     private static String[] commandLineOptions = new String[0];
 
-    /**
-     * Contains all options (recursive one included) that are used for a particular execution
-     */
+    /** Represents the set of all options (recursive one included) that are used for a particular run. */
     private static String optionString = "";
 
-
+    /**
+     * Sets the configuration file name.
+     * @param fileName a {@code String} that represent the input file name.
+     */
     public static void setConfFileName(String fileName){
         confFileName = fileName;
     }
 
+    /**
+     * Sets the options for a given {@code Class}.
+     * @param className a given {@code Class}.
+     * @param args an {@code array} of {@code String}.
+     */
     public static void setArgsOptions(Class className, String[] args){
         setArgsOptions(className.getName(), args);
     }
 
+    /**
+     * Sets the options for a given class name ID.
+     * @param classNameID a {@code String} that represents the class name ID.
+     * @param args an {@code array} of {@code String}.
+     */
     public static void setArgsOptions(String classNameID, String[] args) {
-
 
         commandLineOptions = new String[args.length+1];
         commandLineOptions[0] = subOptionSeparator+classNameID;
         int offset = 0;
-        /*
-         * Check -help and -confFileName (name of the class should be passed) and store args
-         */
+
         for (int i = 0; i < args.length; i++) {
+            // Check -help and -confFileName (name of the class should be passed) and store args
+
             if(args[i].equalsIgnoreCase("-help") || args[i].equalsIgnoreCase("--help")){
                  if(args.length == 1){
                     String options = getListOptions(classNameID);
@@ -64,6 +82,7 @@ public class OptionParser {
                 }
                 System.exit(0);
             }
+
             if(args[i].equalsIgnoreCase("-confFile")) {
                 setConfFileName(args[i++]);
                 offset = 2;
@@ -71,19 +90,26 @@ public class OptionParser {
                 commandLineOptions[i+1-offset] = args[i];
             }
         }
-        if(offset==2) {//Remove two last empty elements
+
+        if(offset==2) {
+            //Remove two last empty elements
             commandLineOptions = new String[commandLineOptions.length-2];
             System.arraycopy(commandLineOptions, 0 , commandLineOptions, 0, commandLineOptions.length-2);
         }
-
     }
 
-
+    /**
+     * Parses the set of all options.
+     * @param classNameID the class name ID.
+     * @param defaultOptions the default options.
+     * @param optionName the option name.
+     * @return a {@code String} containing the set of all options.
+     */
     public static String parse(String classNameID, String defaultOptions, String optionName) {
         /**
          * The preference order is as follows:
-         * 1) Command line options (if provided).
-         * 2) Configuration file options (if found or provided).
+         * 1) Configuration file options (if found or provided).
+         * 2) Command line options (if provided).
          * 3) Options by default (listed in listOptions() in each class).
          * The first time it is called, it must put all options in a String (recursively) and print it at some point.
          */
@@ -100,7 +126,9 @@ public class OptionParser {
         return allOptions.get(classNameID).get(optionName);
     }
 
-
+    /**
+     * Loads the configuration file options.
+     */
     public static void loadFileOptions(){
 
         allOptions = new HashMap<>();
@@ -125,6 +153,10 @@ public class OptionParser {
         }
     }
 
+    /**
+     * Parses a {@code String} line.
+     * @param line a {@code String} line
+     */
     private static void parseLine(String line){
         String[] splitLine = line.split("\\s+|\\s*,\\s*|\\s*=\\s*");
         HashMap<String,String> local = new HashMap<>();
@@ -143,6 +175,10 @@ public class OptionParser {
         }
     }
 
+    /**
+     * Loads the command line options.
+     * @param classNameID the class name ID.
+     */
     public static void loadCommandLineOptions(String classNameID){
 
         String className = classNameID;
@@ -178,6 +214,10 @@ public class OptionParser {
         }
     }
 
+    /**
+     * Loads the set of options by default.
+     * @param defaultOptions s {@code String} containing the set of default options.
+     */
     public static void loadDefaultOptions(String defaultOptions){
 
         String[] options = defaultOptions.split(",\\\\|\\s*,\\s*|\\\\");
@@ -203,6 +243,11 @@ public class OptionParser {
         }
     }
 
+    /**
+     * Returns the list of options of a given a class name ID.
+     * @param classNameID a given a class name ID
+     * @return a {@code String} containing le list of options.
+     */
     public static String getListOptions(String classNameID){
         try {
             return ((AmidstOptionsHandler) Class.forName(classNameID).newInstance()).listOptions();
@@ -211,6 +256,11 @@ public class OptionParser {
         }
     }
 
+    /**
+     * Returns recursively the list of options of a given a class name ID.
+     * @param classNameID a given a class name ID
+     * @return a {@code String} containing le list of options.
+     */
     public static String getListOptionsRecursively(String classNameID){
         try {
             return ((AmidstOptionsHandler) Class.forName(classNameID).newInstance()).listOptionsRecursively();
