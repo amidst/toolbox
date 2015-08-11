70605fa46703a31fe644c69194530c6201fa8ed4
Hanen Borchani <hanene.borchani@gmail.com>
2015-08-11 13:13:54 +0200

    update javadoc

--- core/src/main/java/eu/amidst/core/inference/InferenceAlgorithm.java
+++ core/src/main/java/eu/amidst/core/inference/InferenceAlgorithm.java
@@ -18,29 +18,49 @@ import java.util.function.Function;
 import java.util.stream.IntStream;
 
 /**
- *
- * <p> For an example of use follow this link </p>
- * <p> <a href="http://amidst.github.io/toolbox/#inferenceexample"> http://amidst.github.io/toolbox/#inferenceexample </a>  </p>
+ * This interface handles and defines the algorithm used to run inference in {@link BayesianNetwork} models.
+ * For an example, {@see eu.amidst.core.examples.inference}.
  */
 public interface InferenceAlgorithm {
 
+    /**
+     * Runs inference.
+     */
     void runInference();
 
+    /**
+     * Sets the model for this InferenceAlgorithm.
+     * @param model a {@link BayesianNetwork} model to which this InferenceAlgorithm will be set.
+     */
     void setModel(BayesianNetwork model);
 
+    /**
+     * Returns the original model of this InferenceAlgorithm.
+     * @return a {@link BayesianNetwork} object that represents the original model of this InferenceAlgorithm.
+     */
     BayesianNetwork getOriginalModel();
 
+    /**
+     * Sets the evidence for this InferenceAlgorithm.
+     * @param assignment an {@link Assignment} object to which the evidence will be set.
+     */
     void setEvidence(Assignment assignment);
 
     /**
-     * This method is only implemented for those inference
-     * algorithms which can be run in parallel.
-     * @param parallelMode_
+     * Sets the parallel mode for this InferenceAlgorithm.
+     * Note that this method is only implemented for the inference algorithms that can be run in parallel.
+     * @param parallelMode_ a {@code boolean} that indicates whether this InferenceAlgorithm can be run in parallel (i.e., true) or not (i.e., false).
      */
     default void setParallelMode(boolean parallelMode_){
 
     }
 
+    /**
+     * Returns the expected value.
+     * @param var a {@link Variable} object.
+     * @param function a {@code Function} object.
+     * @return a {@code double} that represents the expected value.
+     */
     default double getExpectedValue(Variable var, Function<Double,Double> function){
         UnivariateDistribution univariateDistribution = this.getPosterior(var);
 
@@ -52,13 +72,31 @@ public interface InferenceAlgorithm {
         return val/nSamples;
     }
 
+    /**
+     * Returns the posterior of a given {@link Variable}.
+     * @param var a {@link Variable} object.
+     * @return an {@link UnivariateDistribution} object.
+     */
     <E extends UnivariateDistribution> E getPosterior(Variable var);
 
+    /**
+     * Returns the posterior of a given {@link Variable}.
+     * @param varID an {@code int} that represents the ID of a variable.
+     * @return an {@link UnivariateDistribution} object.
+     */
     default <E extends UnivariateDistribution> E getPosterior(int varID){
         return this.getPosterior(this.getOriginalModel().getStaticVariables().getVariableById(varID));
     }
 
+    /**
+     * Returns the log probability of the evidence.
+     * @return the log probability of the evidence.
+     */
     double getLogProbabilityOfEvidence();
 
+    /**
+     * Sets the seed.
+     * @param seed an {@code int} that represents the seed value to be set.
+     */
     void setSeed(int seed);
 }
