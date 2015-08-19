77230931da61b46b4853e62fe9c0eee4e161f0f7
Hanen Borchani <hanene.borchani@gmail.com>
2015-08-19 16:07:31 +0200

    no message

--- moalink/src/main/java/eu/amidst/moalink/converterFromMoaToAmidst/DataRowWeka.java
+++ moalink/src/main/java/eu/amidst/moalink/converterFromMoaToAmidst/DataRowWeka.java
@@ -6,34 +6,55 @@ import eu.amidst.core.datastream.filereaders.DataRow;
 import weka.core.Instance;
 
 /**
- * Created by ana@cs.aau.dk on 14/11/14.
+ * This class implements the interface {@link DataRow} and defines a row of the data in WEKA format.
+ * <p> For more information about WEKA Attribute-Relation File Format (ARFF),
+ * take a look at http://www.cs.waikato.ac.nz/ml/weka/arff.html </p>
  */
 public class DataRowWeka implements DataRow {
 
+    /** Represents an {@link weka.core.Instance} object. */
     private Instance dataRow;
+
+    /** Represents a set of {@link Attributes}. */
     private Attributes attributes;
 
-
+    /**
+     * Creates a new DataRowWeka from a given instance and a set of attributes.
+     * @param dataRow an {@link weka.core.Instance} object.
+     * @param attributes_ a set of {@link Attributes}.
+     */
     public DataRowWeka(Instance dataRow, Attributes attributes_){
             this.dataRow = dataRow;
             this.attributes = attributes_;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public double getValue(Attribute att) {
         return dataRow.value(att.getIndex());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setValue(Attribute att, double value) {
         dataRow.setValue(att.getIndex(), value);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Attributes getAttributes() {
         return this.attributes;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public double[] toArray() {
         return dataRow.toDoubleArray();
