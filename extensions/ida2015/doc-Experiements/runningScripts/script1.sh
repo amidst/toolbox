8b53bdc1f4fed7fa17db1c4a4cd1e72cbcf058e3
Hanen Borchani <hanene.borchani@gmail.com>
2015-11-30 13:55:43 +0100

    no message

--- extensions/ida2015/doc-Experiements/runningScripts/script1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-java -Xmx5000M  -cp "./*"  moa.DoTask EvaluatePrequential -l \(bayes.amidstModels -d GLOBAL -w 100 -v 0.1\) -s \(ArffFileStream -f \($1\)\) -f 1000 >& $0_global_w100_v0.1.txt &
-java -Xmx5000M  -cp "./*"  moa.DoTask EvaluatePrequential -l \(bayes.amidstModels -d LOCAL -w 100 -v 0.1\) -s \(ArffFileStream -f \($1\)\) -f 1000 >& $0_local_w100_v0.1.txt &
