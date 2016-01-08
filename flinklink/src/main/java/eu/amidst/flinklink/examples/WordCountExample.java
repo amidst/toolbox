17dcf36e94bcc1ac1d1a9256f9070e33ece499f1
anamf <anam.martinezf@gmail.com>
2016-01-08 15:28:39 +0100

    Tests with WordCountExample and Loggers

--- flinklink/src/main/java/eu/amidst/flinklink/examples/WordCountExample.java
+++ flinklink/src/main/java/eu/amidst/flinklink/examples/WordCountExample.java
@@ -23,24 +23,20 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-//import org.apache.log4j.BasicConfigurator;
 //import org.apache.log4j.Logger;
 
 /**
  * Created by andresmasegosa on 1/9/15.
  */
 public class WordCountExample {
-    //static Logger logger = Logger.getLogger(WordCountExample.class);
     static Logger logger = LoggerFactory.getLogger(WordCountExample.class);
 
     public static void main(String[] args) throws Exception {
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-        //BasicConfigurator.configure();
-
         logger.info("Entering application.");
 
-    DataSet<String> text = env.fromElements(
+        DataSet<String> text = env.fromElements(
                 "Who's there?",
                 "I think I hear them. Stand, ho! Who's there?");
 
@@ -50,34 +46,45 @@ public class WordCountExample {
 
         DataSet<TestClass> set = env.fromElements(new TestClass(elements));
 
-        DataSet<Tuple2<String, Integer>> wordCounts = text
-                .flatMap(new LineSplitter())
-                .withBroadcastSet(set, "set")
-                .groupBy(0)
-                .sum(1);
+        try {
+            DataSet<Tuple2<String, Integer>> wordCounts = text
+                    .flatMap(new LineSplitter())
+                    .withBroadcastSet(set, "set")
+                    .groupBy(0)
+                    .sum(1);
+        }catch (RuntimeException e){
+            System.out.println("Exc");
+        };
 
-        wordCounts.print();
-
+        //wordCounts.writeAsText("output.txt", FileSystem.WriteMode.OVERWRITE);
+        /*wordCounts.map(t -> {
+            System.out.println(t.f0 + "\t" + t.f1);
+            return t;
+        }).count();*/
 
     }
+
 
     public static class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
 
         static Logger loggerLineSplitter = LoggerFactory.getLogger(LineSplitter.class);
 
         @Override
-        public void flatMap(String line, Collector<Tuple2<String, Integer>> out) {
+        public void flatMap(String line, Collector<Tuple2<String, Integer>> out) throws RuntimeException{
+
             loggerLineSplitter.info("Logger in LineSplitter.flatMap");
             for (String word : line.split(" ")) {
                 out.collect(new Tuple2<String, Integer>(word, 1));
+                throw new RuntimeException("LineSplitter class called");
             }
+
         }
     }
 
     public static class TestClass implements Serializable {
         private static final long serialVersionUID = -2932037991574118651L;
 
-        static Logger loggerTestClass = LoggerFactory.getLogger("WordCountExample.TestClass");
+        static Logger loggerTestClass = LoggerFactory.getLogger("TestClass.class");
 
         List<Integer> integerList;
         public TestClass(List<Integer> integerList){
