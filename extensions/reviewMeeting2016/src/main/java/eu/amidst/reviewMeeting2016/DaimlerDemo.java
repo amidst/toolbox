4acd71f6a7a3066209fc4f1a4bedc2768f38e051
Helge Langseth <helgel@idi.ntnu.no>
2016-01-22 14:19:10 +0100

    Cosmetics for the DaimlerData-demo

--- extensions/reviewMeeting2016/src/main/java/eu/amidst/reviewMeeting2016/DaimlerDemo.java
+++ extensions/reviewMeeting2016/src/main/java/eu/amidst/reviewMeeting2016/DaimlerDemo.java
@@ -63,50 +63,56 @@ public class DaimlerDemo {
 
         //--------------------- LEARNING PHASE --------------------------------------------------//
 
-        //Load the data in ARFF format
+        //Load the data in Weka's ARFF format
         DataStream<DynamicDataInstance> data = DynamicDataStreamLoader.loadFromFile
-                ("/Users/helgel/Desktop/DaimlerFileAllLabelsRecoded-100000-selection-train.arff");
+                ("/Users/helgel/Desktop/DaimlerFile-AllLabelsRecoded-100000-train.arff");
 
         //Generate a dynamic naive Bayes structure
         DynamicDAG dynamicDAG = dynamicNaiveBayesStructure(data.getAttributes(),"MNVR_RuleLabeled");
 
         //Parameter Learning with Streaming variational Bayes VMP
+        //Set the desired options
         DynamicSVB svb = new DynamicSVB();
+        svb.setWindowsSize(1000);   // Samples handled together
+        svb.setSeed(0);             // For the random initialization
+        svb.setOutput(true);        // Dump learning progress
+        svb.setMaxIter(100);        // Max no. iterations by the variational Bayes message passing
+        svb.setThreshold(0.0001);   // Cnvergence criteria: Monitor lower-bound increase
 
-        //Set the desired options for the svb
-        svb.setWindowsSize(1000);
-        svb.setSeed(0);
-        svb.setOutput(true);
-        svb.setMaxIter(100);
-        svb.setThreshold(0.001);
-
-        //We set the dynamicDAG, the data and start learning
+        //We give the dynamicDAG and the data to the learner
+        // and then we run the learning
         svb.setDynamicDAG(dynamicDAG);
         svb.setDataStream(data);
+        System.out.println("Start learning: Report no.iter and Expecected lower-bound per sequence");
         svb.runLearning();
 
-        //Get the learnt DBN, and modify to include expert knowledge: LC is not followed by LF
-        DynamicBayesianNetwork dbnLearnt = svb.getLearntDBN();
-        Variable classVar = dbnLearnt.getDynamicVariables().getVariableByName("MNVR_RuleLabeled");
-        Multinomial_MultinomialParents dist = dbnLearnt.getConditionalDistributionTimeT(classVar);
+        //Get the learned DBN for the inference engine,
+        // and modify to include expert knowledge:
+        // ** LC is cannot be followed by LF **
+        DynamicBayesianNetwork dbnLearned = svb.getLearntDBN();
+        Variable classVar = dbnLearned.getDynamicVariables().getVariableByName("MNVR_RuleLabeled");
+        Multinomial_MultinomialParents dist = dbnLearned.getConditionalDistributionTimeT(classVar);
         dist.getMultinomial(0).setProbabilities(new double[]{1.0,0.0});
 
         //Print the dynamic model to screen
-        System.out.println(dbnLearnt.toString());
-
+        System.out.println("\nThe learned network:\n============================");
+        System.out.println(dbnLearned.toString());
 
         //--------------------- PREDICTION PHASE --------------------------------------------------//
 
-        //We select HUGIN as the Inference Algorithm. It is accessible through the HuginLink
+        //We select HUGIN as the Inference Algorithm.
+        // It is accessible through the HuginLink (imported)
         InferenceEngineForDBN.setInferenceAlgorithmForDBN(new FactoredFrontierForDBN(new HuginInference()));
 
-        //Then, we set the DBN model and load the test-set
-        InferenceEngineForDBN.setModel(dbnLearnt);
+        //Then, we give the DBN model to the inference engine
+        // and then load the test-set
+        InferenceEngineForDBN.setModel(dbnLearned);
         DataStream<DynamicDataInstance> dataTest = DynamicDataStreamLoader.loadFromFile
-                ("/Users/helgel/Desktop/DaimlerFileAllLabelsRecoded-100000-selection-test.arff");
+                ("/Users/helgel/Desktop/DaimlerFile-AllLabelsRecoded-100000-test.arff");
 
         //We process the first few data sequences and show results
-        data.streamOfBatches(1000).limit(2).forEach( sequence -> {
+        System.out.println("Report P(LC) per Sequence-ID & Time-step:");
+        data.streamOfBatches(1000).limit(1).forEach( sequence -> {
 
             //For each instance of the data sequence
             for (DynamicDataInstance instance : sequence) {
@@ -116,18 +122,17 @@ public class DaimlerDemo {
                     InferenceEngineForDBN.reset();
                 }
 
-                // Remove the class label
+                // Hide the class label from the test-set
                 instance.setValue(classVar, Double.NaN);
 
-                //Set the evidence.
+                //Set the evidence, and then calculate posterior
                 InferenceEngineForDBN.addDynamicEvidence(instance);
-
-                //Run inference
                 InferenceEngineForDBN.runInference();
 
-                //Query the posterior of the target variable and print output
+                //Query the posterior of the target variable and print output to screen
                 Multinomial posterior = InferenceEngineForDBN.getFilteredPosterior(classVar);
-                System.out.println(instance.getSequenceID() + "\t" + posterior.getProbabilityOfState("LANECHANGE"));
+                System.out.println(instance.getSequenceID() + "\t" +
+                        posterior.getProbabilityOfState("LANECHANGE"));
             }
         });
 
