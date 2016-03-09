d4385b40fc2765a9e69e4b22d68f8700f4666005
Rafael Cabañas de Paz <rcabanas@Rafa-mac.local>
2016-03-09 12:03:22 +0100

    added Naive Bayes

---
+++ standardmodels/src/main/java/eu/amidst/standardmodels/classifiers/NaiveBayesClassifier.java
@@ -0,0 +1,142 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package eu.amidst.standardmodels.classifiers;
+
+import eu.amidst.core.datastream.Attributes;
+import eu.amidst.core.datastream.DataInstance;
+import eu.amidst.core.datastream.DataOnMemory;
+import eu.amidst.core.datastream.DataStream;
+import eu.amidst.core.distribution.Multinomial;
+import eu.amidst.core.inference.InferenceAlgorithm;
+import eu.amidst.core.inference.messagepassing.VMP;
+import eu.amidst.core.io.DataStreamLoader;
+import eu.amidst.core.learning.parametric.ParallelMLMissingData;
+import eu.amidst.core.models.BayesianNetwork;
+import eu.amidst.core.models.DAG;
+import eu.amidst.core.utils.DAGGenerator;
+import eu.amidst.core.utils.DataSetGenerator;
+import eu.amidst.core.utils.Utils;
+import eu.amidst.core.variables.StateSpaceTypeEnum;
+import eu.amidst.core.variables.Variable;
+import eu.amidst.standardmodels.Model;
+import eu.amidst.standardmodels.eu.amidst.standardmodels.exceptions.WrongConfigurationException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * The NaiveBayesClassifier class implements the interface {@link Classifier} and defines a Naive Bayes Classifier.
+ */
+public class NaiveBayesClassifier extends Model implements Classifier{
+
+
+    /** class variable */
+    final private Variable classVar;
+
+    /**
+     * Constructor of classifier from a list of attributes (e.g. from a datastream).
+     * @param attributes
+     * @param classVarName
+     * @throws WrongConfigurationException
+     */
+    public NaiveBayesClassifier(Attributes attributes, String classVarName) throws WrongConfigurationException {
+        super(attributes);
+        classVar = vars.getVariableByName(classVarName);
+
+    }
+
+
+    @Override
+    protected void buildDAG() {
+
+        dag = new DAG(vars);
+        dag.getParentSets().stream().filter(w -> !w.getMainVar().equals(classVar)).forEach(w -> w.addParent(classVar));
+
+    }
+
+    @Override
+    public boolean isValidConfiguration(){
+        boolean isValid = true;
+
+
+        long numFinite = vars.getListOfVariables().stream()
+                .filter( v -> v.getStateSpaceTypeEnum().equals(StateSpaceTypeEnum.FINITE_SET))
+                .count();
+
+
+        if(numFinite == 0) {
+            isValid = false;
+            String errorMsg = "It should contain at least 1 discrete variable and the rest shoud be real";
+            this.setErrorMessage(errorMsg);
+
+        }
+
+        return  isValid;
+
+    }
+
+
+    /**
+     * Predicts the class membership probabilities for a given instance.
+     * @param instance the data instance to be classified.
+     * @return an array of doubles containing the estimated membership probabilities of the data instance for each class label.
+     */
+    @Override
+    public double[] predict(DataInstance instance) {
+/*        if (!Utils.isMissingValue(instance.getValue(classVar)))
+            System.out.println("Class Variable can not be set.");
+        this.predictions.setEvidence(instance);
+        this.predictions.runInference();
+        Multinomial multinomial = this.predictions.getPosterior(classVar);
+        return multinomial.getParameters();
+
+  */
+    return null;
+    }
+
+
+    /////// Getters and setters
+
+
+    /**
+     * Method to obtain the class variable
+     * @return object of the type {@link Variable} indicating which is the class variable
+     */
+    public Variable getClassVar() {
+        return classVar;
+    }
+
+
+    //////////// example of use
+
+    public static void main(String[] args) throws WrongConfigurationException {
+
+        DataStream<DataInstance> data = DataSetGenerator.generate(1234,1000, 2, 10);
+
+        System.out.println(data.getAttributes().toString());
+
+
+        String classVarName = "DiscreteVar0";
+
+        NaiveBayesClassifier nb = new NaiveBayesClassifier(data.getAttributes(), classVarName);
+
+        if(nb.isValidConfiguration()) {
+            nb.learnModel(data);
+            for (DataOnMemory<DataInstance> batch : data.iterableOverBatches(100)) {
+                nb.updateModel(batch);
+            }
+            System.out.println(nb.getModel());
+            System.out.println(nb.getDAG());
+        }
+    }
+}
+
+
+
+
