6e24cef6ebed0362c8fdcbcfbe3a0299ea8b05e5
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2016-04-13 13:29:17 +0200

    Update in example

--- examples/src/main/java/eu/amidst/core/examples/learning/SVBByBatchExample.java
+++ examples/src/main/java/eu/amidst/core/examples/learning/SVBByBatchExample.java
@@ -65,7 +65,8 @@ public class SVBByBatchExample {
 
         //Then we show how we can perform parameter learning by a sequential updating of data batches.
         for (DataOnMemory<DataInstance> batch : data.iterableOverBatches(5)){
-            parameterLearningAlgorithm.updateModel(batch);
+            double log_likelhood_of_batch = parameterLearningAlgorithm.updateModel(batch);
+            System.out.println("Log-Likelihood of Batch: "+ log_likelhood_of_batch);
         }
 
         //And we get the model
