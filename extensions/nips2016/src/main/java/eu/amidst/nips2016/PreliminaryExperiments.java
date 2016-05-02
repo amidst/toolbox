80430397185a4be5fb7dfd326f79b02da771bd72
anamf <anam.martinezf@gmail.com>
2016-05-02 16:03:03 +0200

    Updated script for experiments

--- extensions/nips2016/src/main/java/eu/amidst/nips2016/PreliminaryExperiments.java
+++ extensions/nips2016/src/main/java/eu/amidst/nips2016/PreliminaryExperiments.java
@@ -5,6 +5,7 @@ import eu.amidst.core.datastream.DataOnMemory;
 import eu.amidst.core.distribution.Normal;
 import eu.amidst.core.inference.messagepassing.VMP;
 import eu.amidst.core.io.BayesianNetworkLoader;
+import eu.amidst.core.learning.parametric.ParallelMaximumLikelihood;
 import eu.amidst.core.learning.parametric.bayesian.DriftSVB;
 import eu.amidst.core.learning.parametric.bayesian.PopulationVI;
 import eu.amidst.core.learning.parametric.bayesian.SVB;
@@ -13,6 +14,7 @@ import eu.amidst.core.models.BayesianNetwork;
 import eu.amidst.core.utils.BayesianNetworkSampler;
 
 import java.io.PrintWriter;
+import java.util.Random;
 
 /**
  * Created by ana@cs.aau.dk on 02/05/16.
@@ -21,129 +23,175 @@ public class PreliminaryExperiments {
 
     public static BayesianNetwork oneNormalVarBN;
 
-    public static int[] batchSize = {50,100,1000};
-    public static int[] memoryPopulationVI = {50,100,1000};
-    public static int sampleSize = 1000000;
+    //public static int[] batchSize = {50, 100, 1000};
+    //public static int[] memoryPopulationVI = {50, 100, 1000};
+    //public static double[] learningRate = {0.01, 0.1, 0.5};
+    //public static double[] deltaValue = {-5, 0, 0.1, 5};
 
-    public static DriftSVB driftSVB;
+    public static int[] batchSize = {100};
+    public static int[] memoryPopulationVI = {100};
+    public static double[] learningRate = {0.1};
+    public static double[] deltaValue = {-5};
+
+    public static int numIter = 100;
+
     public static SVB svb;
+    public static DriftSVB driftSVB;
     public static PopulationVI populationVI;
     public static StochasticVI stochasticVI;
+    public static ParallelMaximumLikelihood ml;
 
-    public static void driftSVBinit(){
+    public static PrintWriter writerPredLL;
+    public static PrintWriter writerLambda ;
+    public static PrintWriter writerMean;
+    //public static PrintWriter writerPopulationSize;
 
+
+    public static void initSVBLearners(int batchSize, double deltaValue) {
         driftSVB = new DriftSVB();
-        driftSVB.setWindowsSize(batchSize[0]);
+        driftSVB.setWindowsSize(batchSize);
         driftSVB.setSeed(0);
+        driftSVB.setDelta(deltaValue);
         VMP vmp = driftSVB.getPlateuStructure().getVMP();
         vmp.setOutput(false);
         vmp.setTestELBO(true);
         vmp.setMaxIter(1000);
         vmp.setThreshold(0.0001);
-
         driftSVB.setDAG(oneNormalVarBN.getDAG());
-
         driftSVB.initLearning();
-    }
-
-    public static void SVBinit(){
 
         svb = new SVB();
-        svb.setWindowsSize(batchSize[0]);
+        svb.setWindowsSize(batchSize);
         svb.setSeed(0);
-        VMP vmp = svb.getPlateuStructure().getVMP();
+        vmp = svb.getPlateuStructure().getVMP();
         vmp.setOutput(false);
         vmp.setTestELBO(true);
         vmp.setMaxIter(1000);
         vmp.setThreshold(0.0001);
-
         svb.setDAG(oneNormalVarBN.getDAG());
-
         svb.initLearning();
+
     }
 
-    public static void populationVIint(){
+    public static void initVILearners(int batchSize, int memoryPopulationVI, double learningRate){
+
         populationVI = new PopulationVI();
-        populationVI.setWindowSize(memoryPopulationVI[0]);
+        populationVI.setWindowSize(memoryPopulationVI);
         populationVI.setSeed(0);
+        populationVI.setLearningFactor(learningRate);
         VMP vmp = svb.getPlateuStructure().getVMP();
         vmp.setOutput(false);
         vmp.setTestELBO(true);
         vmp.setMaxIter(1000);
         vmp.setThreshold(0.0001);
-
         populationVI.setDAG(oneNormalVarBN.getDAG());
-
         populationVI.initLearning();
-    }
 
-    public static void stochasticVIint(){
-        stochasticVI = new StochasticVI(sampleSize);
-        stochasticVI.setBatchSize(batchSize[0]);
+        stochasticVI = new StochasticVI();
+        stochasticVI.setDataSetSize(numIter*batchSize);
+        stochasticVI.setBatchSize(batchSize);
         stochasticVI.setSeed(0);
-        VMP vmp = svb.getPlateuStructure().getVMP();
+        stochasticVI.setLearningFactor(learningRate);
+        vmp = svb.getPlateuStructure().getVMP();
         vmp.setOutput(false);
         vmp.setTestELBO(true);
         vmp.setMaxIter(1000);
         vmp.setThreshold(0.0001);
-
         stochasticVI.setDAG(oneNormalVarBN.getDAG());
-
         stochasticVI.initLearning();
+
+        maximumLikelihoodInit();
     }
 
-    public static void EscenarioMixedConceptDrift(){
 
+    private static void maximumLikelihoodInit(){
+        ml = new ParallelMaximumLikelihood();
+        ml.setParallelMode(true);
+        ml.setDAG(oneNormalVarBN.getDAG());
+        ml.initLearning();
     }
 
-    public static void printOutput(int it) throws Exception{
 
-
+    public static void printPredLL(DataOnMemory<DataInstance> batch) throws Exception{
+        writerPredLL.println(svb.predictedLogLikelihood(batch)+"\t"+driftSVB.predictedLogLikelihood(batch)+"\t"+
+                "\t"+stochasticVI.predictedLogLikelihood(batch)+"\t"+populationVI.predictedLogLikelihood(batch));
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void printOutput() throws Exception{
 
-        oneNormalVarBN = BayesianNetworkLoader.loadFromFile("./networks/simulated/Normal.bn");
-
-        System.out.println(oneNormalVarBN);
+        BayesianNetwork bnML = ml.getLearntBayesianNetwork();
+        BayesianNetwork bnSVB = svb.getLearntBayesianNetwork();
+        BayesianNetwork bnDriftSVB = driftSVB.getLearntBayesianNetwork();
 
         /**
-         * Define Learning VI techniques
+         * Normal A variable
          */
-        driftSVBinit();
-        SVBinit();
-        populationVIint();
-        stochasticVIint();
+        double meanML = ((Normal)bnML.getConditionalDistribution(oneNormalVarBN.getVariables().getVariableByName("A"))).getMean();
+        double meanSVB = ((Normal)bnSVB.getConditionalDistribution(oneNormalVarBN.getVariables().getVariableByName("A"))).getMean();
+        double meanDriftSVB = ((Normal)bnDriftSVB.getConditionalDistribution(oneNormalVarBN.getVariables().getVariableByName("A"))).getMean();
 
-        /**
-         * Output files for predLL, lambda, mean, population size
-         */
-        PrintWriter writerPredLL = new PrintWriter("predLL.txt", "UTF-8");
-        PrintWriter writerLambda = new PrintWriter("lamda.txt", "UTF-8");
-        PrintWriter writerMean = new PrintWriter("mean.txt", "UTF-8");
-        PrintWriter writerPopulationSize = new PrintWriter("populationSize.txt", "UTF-8");
+        writerMean.println(meanML+"\t"+meanSVB+"\t"+meanDriftSVB);
 
+        //writerPopulationSize.println("\t"+"\t"+"\t"+"\t");
 
-        double pred = 0;
+        writerLambda.println(driftSVB.getLambdaValue());
+    }
 
-        for (int i = 0; i < 100; i++) {
+    public static void scenarioMixedConceptDrift(int batchSize) throws Exception{
 
-            if (false) {
+        for (int i = 0; i < numIter; i++) {
+
+            /**
+             * GRADUAL CONCEPT DRIFT 1-19
+             */
+            if (i>0 && i<20) {
                 Normal normal = oneNormalVarBN.getConditionalDistribution(oneNormalVarBN.getVariables().getVariableByName("A"));
                 normal.setMean(normal.getMean()+5);
                 normal.setVariance(normal.getVariance()+0.5);
+            }
 
+            /**
+             * NO CONCEPT DRIFT 20-30
+             */
+
+            /**
+             * ABRUPT CONCEPT DRIFT 30-40 (%3)
+             */
+
+            if (i>30 && i<40 && i % 3 == 0) {
+                oneNormalVarBN.randomInitialization(new Random(i));
                 //System.out.println(oneNormalVarBN);
+            }
+
+            /**
+             * ABRUPT CONCEPT DRIFT 40-70 (%4)
+             */
+            if (i>40 && i<70 && i % 4 == 0) {
+                oneNormalVarBN.randomInitialization(new Random(i));
+                //System.out.println(oneNormalVarBN);
+            }
+
+            /**
+             * NO CONCEPT DRIFT 70-90
+             */
+
+            /**
+             * GRADUAL CONCEPT DRIFT 90-100
+             */
+            if (i>90 && i<100) {
+                Normal normal = oneNormalVarBN.getConditionalDistribution(oneNormalVarBN.getVariables().getVariableByName("A"));
+                normal.setMean(normal.getMean()-5);
+                normal.setVariance(normal.getVariance()-1);
             }
 
             BayesianNetworkSampler sampler = new BayesianNetworkSampler(oneNormalVarBN);
             sampler.setSeed(i);
 
-            DataOnMemory<DataInstance> batch = sampler.sampleToDataStream(batchSize[0]).toDataOnMemory();
+            DataOnMemory<DataInstance> batch = sampler.sampleToDataStream(batchSize).toDataOnMemory();
 
-            if (i>0)
-                pred+=driftSVB.predictedLogLikelihood(batch);
 
+            if(i>0)
+                printPredLL(batch);
 
             /**
              * Update with all different learning techniques
@@ -153,49 +201,115 @@ public class PreliminaryExperiments {
             populationVI.updateModel(batch);
             stochasticVI.updateModel(batch);
 
-
-            //System.out.println(svb.getLearntBayesianNetwork());
+            /* Learn maximum likelihood to get the real means*/
+            ml.updateModel(batch);
 
             /**
              * Outputs: predLL, lambda, mean, population size
              */
+            printOutput();
 
-            writerPredLL.println(svb.predictedLogLikelihood(batch)+"\t"+svb.predictedLogLikelihood(batch)+"\t"+
-                    "\t"+stochasticVI.predictedLogLikelihood(batch)+"\t"+populationVI.predictedLogLikelihood(batch));
-
-            /* Learn maximum likelihood to get the mean*/
-            writerMean.println(svb+"\t"+svb.predictedLogLikelihood(batch)+"\t"+
-                    "\t"+stochasticVI.predictedLogLikelihood(batch)+"\t"+populationVI.predictedLogLikelihood(batch));
-
-            writerPredLL.println(svb.predictedLogLikelihood(batch)+"\t"+svb.predictedLogLikelihood(batch)+"\t"+
-                    "\t"+stochasticVI.predictedLogLikelihood(batch)+"\t"+populationVI.predictedLogLikelihood(batch));
-
-            writerPredLL.println(svb.predictedLogLikelihood(batch)+"\t"+svb.predictedLogLikelihood(batch)+"\t"+
-                    "\t"+stochasticVI.predictedLogLikelihood(batch)+"\t"+populationVI.predictedLogLikelihood(batch));
-
-
-            //System.out.println("N Iter: " + i + ", " + driftSVB.getLambdaValue());
-
-
-
-            /*if (i > 0 && i % 3 == 0) {
-                assertEquals(0.0, svb.getLambdaValue(), 0.1);
-            } else if (i > 0 && i % 3 != 0) {
-                assertEquals(1.0, svb.getLambdaValue(), 0.1);
-            }*/
         }
+    }
+
+    public static void scenarioNoConceptDrift(int batchSize) throws Exception{
+
+        for (int i = 0; i < numIter; i++) {
+
+            BayesianNetworkSampler sampler = new BayesianNetworkSampler(oneNormalVarBN);
+            sampler.setSeed(i);
+
+            DataOnMemory<DataInstance> batch = sampler.sampleToDataStream(batchSize).toDataOnMemory();
+
+
+            if(i>0)
+                printPredLL(batch);
+
+            /**
+             * Update with all different learning techniques
+             */
+            driftSVB.updateModelWithConceptDrift(batch);
+            svb.updateModel(batch);
+            populationVI.updateModel(batch);
+            stochasticVI.updateModel(batch);
+
+            BayesianNetwork bnSVB = svb.getLearntBayesianNetwork();
+            BayesianNetwork bnStochasticVI = stochasticVI.getLearntBayesianNetwork();
+
+            /* Learn maximum likelihood to get the real means*/
+            ml.updateModel(batch);
+
+            /**
+             * Outputs: predLL, lambda, mean, population size
+             */
+            printOutput();
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
 
         /**
-         * Close all output files
+         * Parameters to test: batchSize,
+         *                     memory in populationIV,
+         *                     delta in initLearning of DriftSVB
+         *                     learningRate in StochasticVB & populationIV
          */
-        writerPredLL.close();
-        writerLambda.close();
-        writerMean.close();
-        writerPopulationSize.close();
+
+        oneNormalVarBN = BayesianNetworkLoader.loadFromFile("./networks/simulated/Normal.bn");
+
+        System.out.println(oneNormalVarBN);
+
+        for (int i = 0; i < batchSize.length; i++) {
+
+            for (int j = 0; j < deltaValue.length; j++) {
+
+                for (int k = 0; k < memoryPopulationVI.length; k++) {
+
+                    for (int l = 0; l < learningRate.length; l++) {
+
+                        /**
+                         * Define Learning VI techniques
+                         */
+                        initSVBLearners(batchSize[i], deltaValue[j]);
+                        initVILearners(batchSize[i], memoryPopulationVI[k], learningRate[l]);
+
+
+                        /**
+                         * Output files for predLL, lambda, mean, population size
+                         */
+                        String path = "extensions/nips2016/doc-Experiments/preliminaryExperiments/";
+                        writerPredLL = new PrintWriter(path + "predLL_" + "bs_" + batchSize[i] + "delta_" +
+                                deltaValue[j]+ "mem_" + memoryPopulationVI[k] + "lr_" + learningRate[i] +
+                                ".txt", "UTF-8");
+                        writerLambda = new PrintWriter(path + "lamda_" + "bs_" + batchSize[i] + "delta_" +
+                                deltaValue[j]+ "mem_" + memoryPopulationVI[k] + "lr_" + learningRate[i] +
+                                ".txt", "UTF-8");
+                        writerMean = new PrintWriter(path + "mean_" + "bs_" + batchSize[i] + "delta_" +
+                                deltaValue[j]+ "mem_" + memoryPopulationVI[k] + "lr_" + learningRate[i] +
+                                ".txt", "UTF-8");
+                        //writerPopulationSize = new PrintWriter("populationSize.txt", "UTF-8");
+
+
+                        /**
+                         * Choose concept drift scenario
+                         */
+                        //scenarioMixedConceptDrift(batchSize[i]);
+                        scenarioNoConceptDrift(batchSize[i]);
+
+                        /**
+                         * Close all output files
+                         */
+                        writerPredLL.close();
+                        writerLambda.close();
+                        writerMean.close();
+                        //writerPopulationSize.close();
+                    }
+                }
+            }
+        }
 
         //System.out.println(driftSVB.getLearntBayesianNetwork());
 
-
-        System.out.println(pred);
     }
 }
