e5599084e981ac32a791239f46f45e37ffde9859
Andrés Masegosa Arredondo <andresmasegosa@gmail.com>
2016-05-02 15:11:52 +0200

    SparseVector added to utils package

---
+++ core/src/main/java/eu/amidst/core/utils/SparseVector.java
@@ -0,0 +1,7 @@
+package eu.amidst.core.utils;
+
+/**
+ * Created by andresmasegosa on 2/5/16.
+ */
+public class SparseVector {
+}
---
+++ core/src/test/java/eu/amidst/core/utils/SparseVectorTest.java
@@ -0,0 +1,10 @@
+package eu.amidst.core.utils;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by andresmasegosa on 2/5/16.
+ */
+public class SparseVectorTest extends TestCase {
+
+}
