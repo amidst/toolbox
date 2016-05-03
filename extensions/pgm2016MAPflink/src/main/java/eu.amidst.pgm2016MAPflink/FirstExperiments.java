9f74e322e4325b2a9905ff346d0a22232524e85f
Darío Ramos López <drl012@ual.es>
2016-05-03 15:33:30 +0200

    Experiments for PGM paper (MAP over Flink)

---
+++ extensions/pgm2016MAPflink/src/main/java/eu.amidst.pgm2016MAPflink/FirstExperiments.java
@@ -0,0 +1,11 @@
+package eu.amidst.pgm2016MAPflink;
+
+import eu.amidst.flinklink.core.inference.DistributedMAPInference;
+
+/**
+ * Created by dario on 3/5/16.
+ */
+public class FirstExperiments {
+
+    DistributedMAPInference distributedMAPInference = new DistributedMAPInference();
+}
