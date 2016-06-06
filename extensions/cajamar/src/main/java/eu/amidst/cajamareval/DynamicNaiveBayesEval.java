560c54162c49bc6252904ba28d242f9aa4b42e3b
Darío Ramos López <drl012@ual.es>
2016-06-06 14:40:00 +0200

    Dynamic models for Cajamar using Flink

---
+++ extensions/cajamar/src/main/java/eu/amidst/cajamareval/DynamicNaiveBayesEval.java
@@ -0,0 +1,29 @@
+package eu.amidst.cajamareval;
+
+import eu.amidst.dynamic.datastream.DynamicDataInstance;
+import eu.amidst.flinklink.core.data.DataFlink;
+import eu.amidst.flinklink.core.io.DataFlinkLoader;
+import org.apache.flink.api.java.ExecutionEnvironment;
+
+import java.io.IOException;
+
+/**
+ * Created by dario on 6/6/16.
+ */
+public class DynamicNaiveBayesEval {
+
+    public static void main(String[] args) throws IOException {
+
+        String fileDay0 = "/Users/dario/Desktop/CAJAMAR_dynamic/ACTIVOS_train/train0.arff";
+        String fileDay1 = "/Users/dario/Desktop/CAJAMAR_dynamic/ACTIVOS_train/train1.arff";
+
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        DataFlink<DynamicDataInstance> dynamicDataInstanceDataFlink =  DataFlinkLoader.loadDynamicDataFromFile(env, fileDay0, true);
+        dynamicDataInstanceDataFlink.getAttributes().forEach(attribute -> System.out.println(attribute.getName()));
+
+        DataFlink<DynamicDataInstance> dynamicDataInstanceDataFlink1 =  DataFlinkLoader.loadDynamicDataFromFile(env, fileDay1, true);
+        dynamicDataInstanceDataFlink1.getAttributes().forEach(attribute -> System.out.println(attribute.getName()));
+
+    }
+}
