2e7624064ce4fa644581e1fb3cdf3d19c5445f3b
Rafael Cabañas de Paz <rafacabanas@gmail.com>
2016-09-27 16:45:31 +0200

    reading JSON files in sparklink

--- examples/src/main/java/eu/amidst/sparklink/examples/io/DataStreamLoaderExample.java
+++ examples/src/main/java/eu/amidst/sparklink/examples/io/DataStreamLoaderExample.java
@@ -8,6 +8,7 @@ import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.SQLContext;
 import eu.amidst.sparklink.core.data.DataSpark;
 import eu.amidst.sparklink.core.io.DataSparkLoader;
+
 /**
  * Created by rcabanas on 10/06/16.
  */
@@ -17,13 +18,16 @@ public class DataStreamLoaderExample {
         SparkContext sc = new SparkContext(conf);
         SQLContext sqlContext = new SQLContext(sc);
 
-        //Paths to datasets
-        String path = "datasets/simulated/syntheticData.arff";
-        DataFrame df = sqlContext.read().format("parquet").load(path);
+        //Paths to dataset
+        String path ="datasets/simulated/WasteIncinerator.json";
 
+		//Load the data and store it into an object of class DataFrame
+        DataFrame df = sqlContext.read().format("json").load(path);
 
+		//Create an AMIDST object for managing the data
         DataSpark dataSpark = DataSparkLoader.loadSparkDataFrame(df);
 
+		//Print all the instances in the dataset
         dataSpark.collectDataStream().forEach(dataInstance -> System.out.println(dataInstance) );
 
 
---
+++ examples/src/main/java/eu/amidst/sparklink/examples/util/SamplingBN.java
@@ -0,0 +1,51 @@
+package eu.amidst.sparklink.examples.util;
+
+import eu.amidst.core.io.BayesianNetworkLoader;
+import eu.amidst.core.models.BayesianNetwork;
+import eu.amidst.sparklink.core.data.DataSpark;
+import eu.amidst.sparklink.core.util.BayesianNetworkSampler;
+import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SQLContext;
+
+import java.io.IOException;
+
+/**
+ * Created by rcabanas on 27/09/16.
+ */
+public class SamplingBN {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+		//Setting up spark
+		SparkConf conf = new SparkConf().setAppName("SparkLink!").setMaster("local");
+		JavaSparkContext jsc = new JavaSparkContext(conf);
+		SQLContext sqlContext = new SQLContext(jsc);
+
+
+		// Open a Bayesian network
+		String file = "networks/simulated/WasteIncinerator.bn";
+		BayesianNetwork bn = BayesianNetworkLoader.loadFromFile(file);
+
+
+
+		// Sample from the BN
+		int nSamples = 1000;
+		int parallelism = 4;
+		BayesianNetworkSampler sampler = new BayesianNetworkSampler(bn);
+		DataSpark data = sampler.sampleToDataSpark(jsc, nSamples, parallelism);
+
+
+		// Save it as a json file
+		data.getDataFrame(sqlContext).write().format("json").save("datasets/simulated/WasteIncinerator.json");
+
+	}
+
+
+
+}
+
+
+
+
