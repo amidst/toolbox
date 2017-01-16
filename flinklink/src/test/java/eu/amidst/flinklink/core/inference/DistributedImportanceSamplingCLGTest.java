15845253833bacc0e623365505f2a4b2edc7b5e6
Darío Ramos López <drl012@ual.es>
2017-01-16 10:05:39 +0100

    Added some basic tests for DistributedImportanceSamplingCLG

---
+++ flinklink/src/test/java/eu/amidst/flinklink/core/inference/DistributedImportanceSamplingCLGTest.java
@@ -0,0 +1,428 @@
+/*
+ *
+ *
+ *    Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.
+ *    See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+ *    The ASF licenses this file to You under the Apache License, Version 2.0 (the "License"); you may not use
+ *    this file except in compliance with the License.  You may obtain a copy of the License at
+ *
+ *            http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software distributed under the License is
+ *    distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and limitations under the License.
+ *
+ *
+ */
+
+package eu.amidst.flinklink.core.inference;
+
+import eu.amidst.core.distribution.*;
+import eu.amidst.core.models.BayesianNetwork;
+import eu.amidst.core.models.DAG;
+import eu.amidst.core.variables.HashMapAssignment;
+import eu.amidst.core.variables.Variable;
+import eu.amidst.core.variables.Variables;
+import junit.framework.TestCase;
+
+public class DistributedImportanceSamplingCLGTest extends TestCase {
+
+    final static double probThreshold = 0.1;
+
+    // A -> B
+    public static void test1() throws Exception {
+
+        Variables variables = new Variables();
+        Variable varA = variables.newMultionomialVariable("A", 2);
+        Variable varB = variables.newMultionomialVariable("B", 2);
+
+        DAG dag = new DAG(variables);
+
+        dag.getParentSet(varB).addParent(varA);
+
+        BayesianNetwork bn = new BayesianNetwork(dag);
+
+        Multinomial distA = bn.getConditionalDistribution(varA);
+        Multinomial_MultinomialParents distB = bn.getConditionalDistribution(varB);
+
+        distA.setProbabilities(new double[]{0.9, 0.1});
+        distB.getMultinomial(0).setProbabilities(new double[]{0.75, 0.25});
+        distB.getMultinomial(1).setProbabilities(new double[]{0.25, 0.75});
+
+        //bn.randomInitialization(new Random(0));
+
+        double[] pA = distA.getProbabilities();
+        double[][] pB = new double[2][];
+        pB[0] = distB.getMultinomial(0).getProbabilities();
+        pB[1] = distB.getMultinomial(1).getProbabilities();
+
+        System.out.println(bn.toString());
+
+
+        HashMapAssignment assignment = new HashMapAssignment(1);
+        assignment.setValue(varB, 1.0);
+
+
+        DistributedImportanceSamplingCLG importanceSampling = new DistributedImportanceSamplingCLG();
+        importanceSampling.setSampleSize(100000);
+        importanceSampling.setModel(bn);
+        importanceSampling.setGaussianMixturePosteriors(false);
+
+        importanceSampling.runInference();
+
+        Multinomial postA = importanceSampling.getPosterior(varA);
+        System.out.println("P(A) = " + postA.toString());
+        Multinomial postB = importanceSampling.getPosterior(varB);
+        System.out.println("P(B) = " + postB.toString());
+
+
+        assertEquals(postA.getProbabilities()[0], 0.9, probThreshold);
+        assertEquals(postB.getProbabilities()[0], 0.7, probThreshold);
+
+    }
+
+    // A -> (B=0.0)
+    public static void test2() throws Exception {
+
+        Variables variables = new Variables();
+        Variable varA = variables.newMultionomialVariable("A", 2);
+        Variable varB = variables.newMultionomialVariable("B", 2);
+
+        DAG dag = new DAG(variables);
+
+        dag.getParentSet(varB).addParent(varA);
+
+        BayesianNetwork bn = new BayesianNetwork(dag);
+
+        Multinomial distA = bn.getConditionalDistribution(varA);
+        Multinomial_MultinomialParents distB = bn.getConditionalDistribution(varB);
+
+        distA.setProbabilities(new double[]{0.5, 0.5});
+        distB.getMultinomial(0).setProbabilities(new double[]{0.75, 0.25});
+        distB.getMultinomial(1).setProbabilities(new double[]{0.25, 0.75});
+
+        System.out.println(bn.toString());
+
+        HashMapAssignment assignment = new HashMapAssignment(1);
+        assignment.setValue(varB, 0.0);
+
+
+        DistributedImportanceSamplingCLG importanceSampling = new DistributedImportanceSamplingCLG();
+        importanceSampling.setSampleSize(100000);
+        importanceSampling.setModel(bn);
+        importanceSampling.setGaussianMixturePosteriors(false);
+
+        importanceSampling.setEvidence(assignment);
+
+        importanceSampling.runInference();
+
+
+        Multinomial postA = importanceSampling.getPosterior(varA);
+        System.out.println("P(A) = " + postA.toString());
+
+        assertEquals(0.75, postA.getProbabilities()[0], probThreshold);
+
+    }
+
+
+    // (A,B) -> C
+    public static void test3() throws Exception {
+
+        Variables variables = new Variables();
+        Variable varA = variables.newMultionomialVariable("A", 2);
+        Variable varB = variables.newMultionomialVariable("B", 2);
+        Variable varC = variables.newMultionomialVariable("C", 2);
+
+        DAG dag = new DAG(variables);
+
+        dag.getParentSet(varC).addParent(varA);
+        dag.getParentSet(varC).addParent(varB);
+
+        BayesianNetwork bn = new BayesianNetwork(dag);
+
+        Multinomial distA = bn.getConditionalDistribution(varA);
+        Multinomial distB = bn.getConditionalDistribution(varB);
+        Multinomial_MultinomialParents distC = bn.getConditionalDistribution(varC);
+
+        distA.setProbabilities(new double[]{0.5, 0.5});
+        distB.setProbabilities(new double[]{0.5, 0.5});
+
+        distC.getMultinomial(0).setProbabilities(new double[]{0.6, 0.4});
+        distC.getMultinomial(1).setProbabilities(new double[]{0.2, 0.8});
+        distC.getMultinomial(2).setProbabilities(new double[]{0.9, 0.1});
+        distC.getMultinomial(3).setProbabilities(new double[]{0.7, 0.3});
+
+        //bn.randomInitialization(new Random(0));
+
+        System.out.println(bn.toString());
+
+
+        DistributedImportanceSamplingCLG importanceSampling = new DistributedImportanceSamplingCLG();
+        importanceSampling.setSampleSize(100000);
+        importanceSampling.setModel(bn);
+        importanceSampling.setGaussianMixturePosteriors(false);
+
+        importanceSampling.runInference();
+
+
+        Multinomial postA = importanceSampling.getPosterior(varA);
+        Multinomial postB = importanceSampling.getPosterior(varB);
+        Multinomial postC = importanceSampling.getPosterior(varC);
+
+
+        System.out.println("P(A) = " + postA.toString());
+        System.out.println("P(B) = " + postB.toString());
+        System.out.println("P(C) = " + postC.toString());
+
+
+        assertEquals(postA.getProbabilities()[0], 0.5, probThreshold);
+        assertEquals(postB.getProbabilities()[0], 0.5, probThreshold);
+        assertEquals(postC.getProbabilities()[0], 0.6, probThreshold);
+
+    }
+
+    // (A,B) -> (C==0)
+    public static void test4() throws Exception {
+
+        Variables variables = new Variables();
+        Variable varA = variables.newMultionomialVariable("A", 2);
+        Variable varB = variables.newMultionomialVariable("B", 2);
+        Variable varC = variables.newMultionomialVariable("C", 2);
+
+        DAG dag = new DAG(variables);
+
+        dag.getParentSet(varC).addParent(varA);
+        dag.getParentSet(varC).addParent(varB);
+
+        BayesianNetwork bn = new BayesianNetwork(dag);
+
+        Multinomial distA = bn.getConditionalDistribution(varA);
+        Multinomial distB = bn.getConditionalDistribution(varB);
+        Multinomial_MultinomialParents distC = bn.getConditionalDistribution(varC);
+
+        distA.setProbabilities(new double[]{0.5, 0.5});
+        distB.setProbabilities(new double[]{0.5, 0.5});
+
+        distC.getMultinomial(0).setProbabilities(new double[]{0.6, 0.4});
+        distC.getMultinomial(1).setProbabilities(new double[]{0.2, 0.8});
+        distC.getMultinomial(2).setProbabilities(new double[]{0.9, 0.1});
+        distC.getMultinomial(3).setProbabilities(new double[]{0.7, 0.3});
+
+        //bn.randomInitialization(new Random(0));
+
+        System.out.println(bn.toString());
+
+
+        HashMapAssignment assignment = new HashMapAssignment(1);
+        assignment.setValue(varC, 0.0);
+
+        DistributedImportanceSamplingCLG importanceSampling = new DistributedImportanceSamplingCLG();
+        importanceSampling.setSampleSize(100000);
+        importanceSampling.setModel(bn);
+        importanceSampling.setGaussianMixturePosteriors(false);
+
+        importanceSampling.setEvidence(assignment);
+
+        importanceSampling.runInference();
+
+
+        Multinomial postA = importanceSampling.getPosterior(varA);
+        Multinomial postB = importanceSampling.getPosterior(varB);
+        Multinomial postC = importanceSampling.getPosterior(varC);
+
+
+        System.out.println("P(A) = " + postA.toString());
+        System.out.println("P(B) = " + postB.toString());
+        System.out.println("P(C) = " + postC.toString());
+
+
+        assertEquals(postA.getProbabilities()[0], 0.625, probThreshold);
+        assertEquals(postB.getProbabilities()[0], 0.333, probThreshold);
+        assertEquals(postC.getProbabilities()[0], 1, 0.001);
+
+    }
+
+
+    // A multinomial -> B Gaussian
+    public static void test5() throws Exception {
+
+        Variables variables = new Variables();
+        Variable varA = variables.newMultionomialVariable("A", 2);
+        Variable varB = variables.newGaussianVariable("B");
+
+        DAG dag = new DAG(variables);
+
+        dag.getParentSet(varB).addParent(varA);
+
+        BayesianNetwork bn = new BayesianNetwork(dag);
+
+        Multinomial distA = bn.getConditionalDistribution(varA);
+        Normal_MultinomialParents distB = bn.getConditionalDistribution(varB);
+
+        distA.setProbabilities(new double[]{0.5, 0.5});
+
+        Normal normal0 = new Normal(varB);
+        Normal normal1 = new Normal(varB);
+
+        normal0.setMean(0);
+        normal0.setVariance(1);
+
+        normal1.setMean(5);
+        normal1.setVariance(1);
+
+        distB.setNormal(0, normal0);
+        distB.setNormal(1, normal1);
+
+
+        //bn.randomInitialization(new Random(0));
+
+        System.out.println(bn.toString());
+
+
+        DistributedImportanceSamplingCLG importanceSampling = new DistributedImportanceSamplingCLG();
+        importanceSampling.setSampleSize(100000);
+        importanceSampling.setModel(bn);
+        importanceSampling.setGaussianMixturePosteriors(false);
+
+
+        importanceSampling.runInference();
+
+
+        Multinomial postA = importanceSampling.getPosterior(varA);
+        Normal postB = importanceSampling.getPosterior(varB);
+
+
+        System.out.println("P(A) = " + postA.toString());
+        System.out.println("P(B) = " + postB.toString());
+
+        assertEquals(0.5, postA.getProbabilities()[0], probThreshold);
+        assertEquals(2.5, postB.getMean(), 0.8);
+        assertEquals(7.25, postB.getVariance(),  3);
+
+
+    }
+
+    // A  multinomial -> B  Gaussian
+    public static void test6() throws Exception {
+
+        Variables variables = new Variables();
+        Variable varA = variables.newMultionomialVariable("A", 2);
+        Variable varB = variables.newGaussianVariable("B");
+
+        DAG dag = new DAG(variables);
+
+        dag.getParentSet(varB).addParent(varA);
+
+        BayesianNetwork bn = new BayesianNetwork(dag);
+
+        Multinomial distA = bn.getConditionalDistribution(varA);
+        Normal_MultinomialParents distB = bn.getConditionalDistribution(varB);
+
+        distA.setProbabilities(new double[]{0.333, 0.667});
+
+        Normal normal0 = new Normal(varB);
+        normal0.setMean(0);
+        normal0.setVariance(2);
+
+        Normal normal1 = new Normal(varB);
+        normal1.setMean(0);
+        normal1.setVariance(3);
+
+        distB.setNormal(0, normal0);
+        distB.setNormal(1, normal1);
+
+
+        //bn.randomInitialization(new Random(0));
+
+        System.out.println(bn.toString());
+
+
+        DistributedImportanceSamplingCLG importanceSampling = new DistributedImportanceSamplingCLG();
+        importanceSampling.setSampleSize(100000);
+        importanceSampling.setModel(bn);
+        importanceSampling.setGaussianMixturePosteriors(false);
+
+        importanceSampling.runInference();
+
+
+        Multinomial postA = importanceSampling.getPosterior(varA);
+        Normal postB = importanceSampling.getPosterior(varB);
+
+
+        System.out.println("P(A) = " + postA.toString());
+        System.out.println("P(B) = " + postB.toString());
+
+        assertEquals(0.333, postA.getProbabilities()[0], probThreshold);
+        assertEquals(0, postB.getMean(), 0.2);
+        assertEquals(2.666, postB.getVariance(), 2);
+
+    }
+
+
+    // A  multinomial -> B  mixture of Gaussians
+    public static void test7() throws Exception {
+
+        Variables variables = new Variables();
+        Variable varA = variables.newMultionomialVariable("A", 2);
+        Variable varB = variables.newGaussianVariable("B");
+
+        DAG dag = new DAG(variables);
+
+        dag.getParentSet(varB).addParent(varA);
+
+        BayesianNetwork bn = new BayesianNetwork(dag);
+
+        Multinomial distA = bn.getConditionalDistribution(varA);
+        Normal_MultinomialParents distB = bn.getConditionalDistribution(varB);
+
+        distA.setProbabilities(new double[]{0.333, 0.667});
+
+        Normal normal0 = new Normal(varB);
+        Normal normal1 = new Normal(varB);
+
+        normal0.setMean(0);
+        normal0.setVariance(1);
+
+        normal1.setMean(10);
+        normal1.setVariance(1);
+
+        distB.setNormal(0, normal0);
+        distB.setNormal(1, normal1);
+
+
+        //bn.randomInitialization(new Random(0));
+
+        System.out.println(bn.toString());
+
+
+        DistributedImportanceSamplingCLG importanceSampling = new DistributedImportanceSamplingCLG();
+        importanceSampling.setSampleSize(100000);
+        importanceSampling.setModel(bn);
+        importanceSampling.setGaussianMixturePosteriors(true);
+        importanceSampling.setMixtureOfGaussiansInitialVariance(3);
+        //importanceSampling.setMixtureOfGaussiansNoveltyRate(0.0001);
+
+
+        importanceSampling.runInference();
+
+
+        Multinomial postA = importanceSampling.getPosterior(varA);
+        GaussianMixture postB = importanceSampling.getPosterior(varB);
+
+
+        System.out.println("P(A) = " + postA.toString());
+        System.out.println("P(B) = " + postB.toString());
+
+        assertEquals(0.333, postA.getProbabilities()[0], probThreshold);
+
+        assertEquals(0.333, postB.getParameters()[0], probThreshold);
+        assertEquals(0, postB.getParameters()[1], 0.1);
+        assertEquals(1, postB.getParameters()[2], 0.5);
+
+        assertEquals(0.666, postB.getParameters()[3], probThreshold);
+        assertEquals(10, postB.getParameters()[4], 0.1);
+        assertEquals(1, postB.getParameters()[5], 0.5);
+
+    }
+
+}
